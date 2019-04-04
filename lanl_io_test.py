28b06a5e65d6bf55c81569493eefcf6eed3eeeab
mhmerrill <mhmerrill@mac.com>
2019-04-04 11:59:09 -0400

    changed mike's lanl i/o test script

--- lanl_io_test.py
+++ lanl_io_test.py
@@ -23,9 +23,9 @@ ak.set_defaults()
 ak.v = False
 if len(sys.argv) > 1:
     ak.connect(server=sys.argv[1], port=sys.argv[2])
-    hdffiles = [sys.argv[3]]
+    hdffiles = [sys.argv[3:]]
 else:
-    print("usage: lanl_io_test server port file")
+    print("usage: lanl_io_test server port files...")
     sys.exit(1)
 
 fields = ['srcIP', 'dstIP', 'srcPort', 'dstPort', 'start']
