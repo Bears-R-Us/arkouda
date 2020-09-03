dcb19e6093f46b60c633aa649d1805b6aa3ef299
mhmerrill <mhmerrill@mac.com>
2020-09-03 19:55:56 -0400

    update registration doc strings

--- arkouda/registration.py
+++ arkouda/registration.py
@@ -10,7 +10,6 @@ __all__ = ["register_pda","attach_pda","unregister_pda"]
 def register_pda(pda, user_defined_name):
     """
     Return a pdarray with a user defined name in the arkouda server so it can be attached to later using attach_pda()
-    Note: registered names/pdarrays in the server are immune to deletion until they are unregistered.
     
     Parameters
     ----------
@@ -27,6 +26,10 @@ def register_pda(pda, user_defined_name):
     See also
     --------
     attach_pda, unregister_pda
+
+    Notes
+    -----
+    Registered names/pdarrays in the server are immune to deletion until they are unregistered.
 
     Examples
     --------
@@ -56,7 +59,6 @@ def register_pda(pda, user_defined_name):
 def attach_pda(user_defined_name):
     """
     Return a pdarray attached to the a registered name in the arkouda server which was registered using register_pda()
-    Note: registered names/pdarrays in the server are immune to deletion until they are unregistered.
     
     Parameters
     ----------
@@ -71,6 +73,10 @@ def attach_pda(user_defined_name):
     See also
     --------
     register_pda, unregister_pda
+
+    Notes
+    -----
+    Registered names/pdarrays in the server are immune to deletion until they are unregistered.
 
     Examples
     --------
@@ -91,7 +97,6 @@ def attach_pda(user_defined_name):
 def unregister_pda(pda):
     """
     Unregister a pdarray in the arkouda server which was previously regisgersted using register_pda() and/or attahced to using attach_pda()
-    Note: registered names/pdarrays in the server are immune to deletion until they are unregistered.
     
     Parameters
     ----------
@@ -105,6 +110,10 @@ def unregister_pda(pda):
     See also
     --------
     register_pda, unregister_pda
+
+    Notes
+    -----
+    Registered names/pdarrays in the server are immune to deletion until they are unregistered.
 
     Examples
     --------
