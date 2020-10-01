e553477deb8eaf4e12be6343846554919c233011
John Yost <hokigeek2@gmail.com>
2020-10-01 15:33:59 -0400

    Updated per PR feedback #450

--- tests/dtypes_tests.py
+++ tests/dtypes_tests.py
@@ -65,5 +65,5 @@ class DtypesTest(ArkoudaTest):
         
     def test_pdarrays_datatypes(self):
         self.assertEqual(dtypes.dtype('float64'), ak.ones(10).dtype)
-        self.assertEqual(dtypes.dtype('uint8'), 
+        self.assertEqual(dtypes.dtype('str_'), 
                          ak.array(['string {}'.format(i) for i in range(0,10)]).dtype)
