3ef319558e14c3c381c8599dae6582a7e7f34f09
hokiegeek2 <hokiegeek2@gmail.com>
2020-12-01 15:38:44 -0500

    integrated check.py into pytest test harness #593

--- tests/check.py
+++ tests/check.py
@@ -7,20 +7,11 @@ import gc
 import sys
 
 from context import arkouda as ak
-
-print(">>> Sanity checks on the arkouda_server")
-
-ak.verbose = False
-if len(sys.argv) > 1:
-    ak.connect(server=sys.argv[1], port=sys.argv[2])
-else:
-    ak.connect()
-
+from base_test import ArkoudaTest
 
 N = 1_000_000
-
-
 errors = False
+
 def pass_fail(f):
     global errors
     errors = errors or not f
@@ -39,8 +30,6 @@ def check_bool(N):
     correct = correct and (d and 5)
     return pass_fail(correct)
 
-print("check boolean :", check_bool(N))
-
 def check_arange(N):
     # create np version
     a = np.arange(N)
@@ -50,8 +39,6 @@ def check_arange(N):
     c = a == b.to_ndarray()
     # print(type(c),c)
     return pass_fail(c.all())
-    
-print("check arange :", check_arange(N))
 
 def check_linspace(N):
     # create np version
@@ -61,8 +48,6 @@ def check_linspace(N):
     # print(a,b)
     f = np.allclose(a, b.to_ndarray())
     return pass_fail(f)
-
-print("check linspace :", check_linspace(N))
 
 def check_ones(N):
     # create np version
@@ -74,8 +59,6 @@ def check_ones(N):
     # print(type(c),c)
     return pass_fail(c.all())
 
-print("check ones :", check_ones(N))
-
 def check_zeros(N):
     # create np version
     a = np.zeros(N)
@@ -85,8 +68,6 @@ def check_zeros(N):
     c = a == b.to_ndarray()
     # print(type(c),c)
     return pass_fail(c.all())
-
-print("check zeros :", check_zeros(N))
 
 def check_argsort(N):
     # create np version
@@ -104,8 +85,6 @@ def check_argsort(N):
     # print(type(c),c)
     return pass_fail(c.all())
 
-print("check argsort :", check_argsort(N))
-
 def check_coargsort(N):
     # create np version
     a = np.arange(N)
@@ -122,8 +101,6 @@ def check_coargsort(N):
     # print(type(c),c)
     return pass_fail(c.all())
 
-print("check coargsort :", check_coargsort(N))
-
 def check_sort(N):
     # create np version
     a = np.arange(N)
@@ -138,8 +115,6 @@ def check_sort(N):
     # print(type(c),c)
     return pass_fail(c.all())
 
-print("check sort :", check_sort(N))
-
 def check_get_slice(N):
     # create np version
     a = np.ones(N)
@@ -150,8 +125,6 @@ def check_get_slice(N):
     # print(a,b)
     c = a == b.to_ndarray()
     return pass_fail(c.all())
-
-print("check get slice [::2] :", check_get_slice(N))
 
 def check_set_slice_value(N):
     # create np version
@@ -164,8 +137,6 @@ def check_set_slice_value(N):
     c = a == b.to_ndarray()
     return pass_fail(c.all())
 
-print("check set slice [::2] = value:", check_set_slice_value(N))
-
 def check_set_slice(N):
     # create np version
     a = np.ones(N)
@@ -176,8 +147,6 @@ def check_set_slice(N):
     # print(a,b)
     c = a == b.to_ndarray()
     return pass_fail(c.all())
-
-print("check set slice [::2] = pda:", check_set_slice(N))
 
 def check_get_bool_iv(N):
     # create np version
@@ -191,8 +160,6 @@ def check_get_bool_iv(N):
     # print(type(c),c)
     return pass_fail(c.all())
 
-print("check (compressing) get bool iv :", check_get_bool_iv(N))
-
 def check_set_bool_iv_value(N):
     # create np version
     a = np.arange(N)
@@ -205,8 +172,6 @@ def check_set_bool_iv_value(N):
     # print(type(c),c)
     return pass_fail(c.all())
 
-print("check (expanding) set bool iv = value:", check_set_bool_iv_value(N))
-
 def check_set_bool_iv(N):
     # create np version
     a = np.arange(N)
@@ -218,8 +183,6 @@ def check_set_bool_iv(N):
     c = a == b.to_ndarray()
     # print(type(c),c)
     return pass_fail(c.all())
-
-print("check (expanding) set bool iv = pda:", check_set_bool_iv(N))
 
 def check_get_integer_iv(N):
     # create np version
@@ -235,8 +198,6 @@ def check_get_integer_iv(N):
     # print(type(c),c)
     return pass_fail(c.all())
 
-print("check (gather) get integer iv:", check_get_integer_iv(N))
-
 def check_set_integer_iv_value(N):
     # create np version
     a = np.arange(N)
@@ -250,8 +211,6 @@ def check_set_integer_iv_value(N):
     c = a == b.to_ndarray()
     # print(type(c),c)
     return pass_fail(c.all())
-
-print("check (scatter) set integer iv = value:", check_set_integer_iv_value(N))
 
 def check_set_integer_iv(N):
     # create np version
@@ -267,8 +226,6 @@ def check_set_integer_iv(N):
     # print(type(c),c)
     return pass_fail(c.all())
 
-print("check (scatter) set integer iv = pda:", check_set_integer_iv(N))
-
 def check_get_integer_idx(N):
     # create np version
     a = np.arange(N)
@@ -277,8 +234,6 @@ def check_get_integer_idx(N):
     b = ak.arange(N)
     v2 = b[N//2]
     return pass_fail(v1 == v2) and pass_fail(a[-1] == b[-1])
-
-print("check get integer idx :", check_get_integer_idx(N))
 
 def check_set_integer_idx(N):
     # create np version
@@ -293,7 +248,99 @@ def check_set_integer_idx(N):
     v2 = b[N//2]
     return pass_fail(v1 == v2) and pass_fail(a[-1] == b[-1])
 
-print("check set integer idx = value:", check_set_integer_idx(N))
+'''
+Encapsulates test cases that invoke the run_tests method.
+'''
+class CheckTest(ArkoudaTest):
+    
+    def testBool(self):
+        self.assertTrue(check_bool(N))
+    
+    def testArange(self):
+        self.assertTrue(check_arange(N))
+        
+    def testLinspace(self):
+        self.assertTrue(check_linspace(N))
+        
+    def testOnes(self):
+        self.assertTrue(check_ones(N))
+        
+    def testZeros(self):
+        self.assertTrue(check_zeros(N))
+        
+    def testArgsort(self):
+        self.assertTrue(check_argsort(N))
+        
+    def testCoargsort(self):
+        self.assertTrue(check_coargsort(N))
+        
+    def testSort(self):
+        self.assertTrue(check_sort(N))
+        
+    def testGetSlice(self):
+        self.assertTrue(check_get_slice(N))
+        
+    def testSetSliceValue(self):
+        self.assertTrue(check_set_slice_value(N))
+        
+    def testSetSlice(self):
+        self.assertTrue(check_set_slice(N))
+        
+    def testGetBoolIv(self):
+        self.assertTrue(check_get_bool_iv(N))
+        
+    def testGetBoolIvValue(self):
+        self.assertTrue(check_set_bool_iv_value(N))
+        
+    def testSetBoolIv(self):
+        self.assertTrue(check_set_bool_iv(N))
+        
+    def testGetIntegerIv(self):
+        self.assertTrue(check_get_integer_iv(N))
+        
+    def testSetIntegerIvValue(self):
+        self.assertTrue(check_set_integer_iv_value(N))
+        
+    def testSetIntegerIv(self):
+        self.assertTrue(check_set_integer_iv(N))
+        
+    def testGetIntegerIdx(self):
+        self.assertTrue(check_get_integer_idx(N))
+        
+    def testSetIntegerIdx(self):
+        self.assertTrue(check_set_integer_idx(N))
 
-ak.disconnect()
-sys.exit(errors)
+if __name__ == '__main__':
+    N = 1_000_000
+    print(">>> Sanity checks on the arkouda_server")
+
+    ak.verbose = False
+    if len(sys.argv) > 1:
+        ak.connect(server=sys.argv[1], port=sys.argv[2])
+    else:
+        ak.connect()
+
+    # Run Tests
+    print("check boolean :", check_bool(N))
+    print("check arange :", check_arange(N))
+    print("check linspace :", check_linspace(N))
+    print("check ones :", check_ones(N))
+    print("check zeros :", check_zeros(N))
+    print("check argsort :", check_argsort(N))
+    print("check coargsort :", check_coargsort(N))
+    print("check sort :", check_sort(N))
+    print("check get slice [::2] :", check_get_slice(N))
+    print("check set slice [::2] = value:", check_set_slice_value(N))
+    print("check set slice [::2] = pda:", check_set_slice(N))
+    print("check (compressing) get bool iv :", check_get_bool_iv(N))
+    print("check (expanding) set bool iv = value:", check_set_bool_iv_value(N))
+    print("check (expanding) set bool iv = pda:", check_set_bool_iv(N))
+    print("check (gather) get integer iv:", check_get_integer_iv(N))
+    print("check (scatter) set integer iv = value:", check_set_integer_iv_value(N))
+    print("check (scatter) set integer iv = pda:", check_set_integer_iv(N))
+    print("check get integer idx :", check_get_integer_idx(N))
+    print("check set integer idx = value:", check_set_integer_idx(N))
+
+    ak.disconnect()
+    sys.exit(errors)
+    
