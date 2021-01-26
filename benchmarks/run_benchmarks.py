e17be26ab4b974713cfb9f53060c3c4989dff6c2
Elliot Ronaghan <ronawho@gmail.com>
2021-01-26 16:49:43 -0500

    Add a string argsort benchmark

--- benchmarks/run_benchmarks.py
+++ benchmarks/run_benchmarks.py
@@ -20,7 +20,7 @@ from util import *
 logging.basicConfig(level=logging.INFO)
 
 BENCHMARKS = ['stream', 'argsort', 'coargsort', 'groupby', 'gather', 'scatter', 'reduce',
-              'scan', 'noop', 'setops', 'array_create', 'IO']
+              'scan', 'noop', 'setops', 'array_create', 'IO', 'str-argsort']
 
 def get_chpl_util_dir():
     """ Get the Chapel directory that contains graph generation utilities. """
