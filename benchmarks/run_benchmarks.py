c5ac4b16bc3f96b8c291891a0a052ac9ab5a1405
Elliot Ronaghan <ronawho@gmail.com>
2021-02-03 13:17:44 -0500

    Add a string coargsort benchmark

--- benchmarks/run_benchmarks.py
+++ benchmarks/run_benchmarks.py
@@ -19,9 +19,9 @@ from util import *
 
 logging.basicConfig(level=logging.INFO)
 
-BENCHMARKS = ['stream', 'argsort', 'coargsort', 'groupby', 'gather', 'scatter', 'reduce',
-              'scan', 'noop', 'setops', 'array_create', 'IO', 'str-argsort',
-              'str-groupby', 'str-gather']
+BENCHMARKS = ['stream', 'argsort', 'coargsort', 'groupby', 'gather', 'scatter',
+              'reduce', 'scan', 'noop', 'setops', 'array_create', 'IO',
+              'str-argsort', 'str-coargsort', 'str-groupby', 'str-gather']
 
 def get_chpl_util_dir():
     """ Get the Chapel directory that contains graph generation utilities. """
