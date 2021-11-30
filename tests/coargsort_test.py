611785b45a1beacb27586b9deb1dc55a25920f10
Bill Reus <reuster986@gmail.com>
2021-11-30 17:19:24 -0500

    command-line test for coargsort with all algorithm choices

--- tests/coargsort_test.py
+++ tests/coargsort_test.py
@@ -5,7 +5,7 @@ import numpy as np
 from context import arkouda as ak 
 from base_test import ArkoudaTest
 
-def check_int(N):
+def check_int(N, algo):
     z = ak.zeros(N, dtype=ak.int64)
 
     a2 = ak.randint(0, 2**16, N)
@@ -14,22 +14,22 @@ def check_int(N):
     d2 = ak.randint(0, 2**16, N)
     n2 = ak.randint(-(2**15), 2**15, N)
 
-    perm = ak.coargsort([a2])
+    perm = ak.coargsort([a2], algo)
     assert ak.is_sorted(a2[perm])
 
-    perm = ak.coargsort([n2])
+    perm = ak.coargsort([n2], algo)
     assert ak.is_sorted(n2[perm])
 
-    perm = ak.coargsort([a2, b2, c2, d2])
+    perm = ak.coargsort([a2, b2, c2, d2], algo)
     assert ak.is_sorted(a2[perm])
 
-    perm = ak.coargsort([z, b2, c2, d2])
+    perm = ak.coargsort([z, b2, c2, d2], algo)
     assert ak.is_sorted(b2[perm])
 
-    perm = ak.coargsort([z, z, c2, d2])
+    perm = ak.coargsort([z, z, c2, d2], algo)
     assert ak.is_sorted(c2[perm])
 
-    perm = ak.coargsort([z, z, z, d2])
+    perm = ak.coargsort([z, z, z, d2], algo)
     assert ak.is_sorted(d2[perm])
 
 
@@ -37,16 +37,16 @@ def check_int(N):
     b4 = ak.randint(0, 2**32, N)
     n4 = ak.randint(-(2**31), 2**31, N)
 
-    perm = ak.coargsort([a4])
+    perm = ak.coargsort([a4], algo)
     assert ak.is_sorted(a4[perm])
 
-    perm = ak.coargsort([n4])
+    perm = ak.coargsort([n4], algo)
     assert ak.is_sorted(n4[perm])
 
-    perm = ak.coargsort([a4, b4])
+    perm = ak.coargsort([a4, b4], algo)
     assert ak.is_sorted(a4[perm])
 
-    perm = ak.coargsort([b4, a4])
+    perm = ak.coargsort([b4, a4], algo)
     assert ak.is_sorted(b4[perm])
 
 
@@ -55,55 +55,55 @@ def check_int(N):
     n8 = ak.randint(-(2**63), 2**64, N)
  
 
-    perm = ak.coargsort([a8])
+    perm = ak.coargsort([a8], algo)
     assert ak.is_sorted(a8[perm])
 
-    perm = ak.coargsort([n8])
+    perm = ak.coargsort([n8], algo)
     assert ak.is_sorted(n8[perm])
 
-    perm = ak.coargsort([b8, a8])
+    perm = ak.coargsort([b8, a8], algo)
     assert ak.is_sorted(b8[perm])
 
     from itertools import permutations
 
     all_perm = permutations([a2, a4, a8])
     for p in all_perm:
-        perm = ak.coargsort(p)
+        perm = ak.coargsort(p, algo)
         assert ak.is_sorted(p[0][perm])
 
-def check_float(N):
+def check_float(N, algo):
     a = ak.randint(0, 1, N, dtype=ak.float64)
     n = ak.randint(-1, 1, N, dtype=ak.float64)
     z = ak.zeros(N, dtype=ak.float64)
 
-    perm = ak.coargsort([a])
+    perm = ak.coargsort([a], algo)
     assert ak.is_sorted(a[perm])
 
-    perm = ak.coargsort([a, n])
+    perm = ak.coargsort([a, n], algo)
     assert ak.is_sorted(a[perm])
 
-    perm = ak.coargsort([n, a])
+    perm = ak.coargsort([n, a], algo)
     assert ak.is_sorted(n[perm])
 
-    perm = ak.coargsort([z, a])
+    perm = ak.coargsort([z, a], algo)
     assert ak.is_sorted(a[perm])
 
-    perm = ak.coargsort([z, n])
+    perm = ak.coargsort([z, n], algo)
     assert ak.is_sorted(n[perm])
 
-def check_int_float(N):
+def check_int_float(N, algo):
     f = ak.randint(0, 2**63, N, dtype=ak.float64)
     i = ak.randint(0, 2**63, N, dtype=ak.int64)
 
-    perm = ak.coargsort([f, i])
+    perm = ak.coargsort([f, i], algo)
     assert ak.is_sorted(f[perm])
 
-    perm = ak.coargsort([i, f])
+    perm = ak.coargsort([i, f], algo)
     assert ak.is_sorted(i[perm])
 
-def check_large(N):
+def check_large(N, algo):
     l = [ak.randint(0, 2**63, N) for _ in range(10)]
-    perm = ak.coargsort(l)
+    perm = ak.coargsort(l, algo)
     assert ak.is_sorted(l[0][perm])
 
 def check_coargsort(N_per_locale):
@@ -112,10 +112,11 @@ def check_coargsort(N_per_locale):
     N = N_per_locale * cfg["numLocales"]
     print("numLocales = {}, N = {:,}".format(cfg["numLocales"], N))
 
-    check_int(N)
-    check_float(N)
-    check_int_float(N)
-    check_large(N)
+    for algo in ak.SortingAlgorithm:
+        check_int(N, algo)
+        check_float(N, algo)
+        check_int_float(N, algo)
+        check_large(N, algo)
 
 class CoargsortTest(ArkoudaTest):
 
