3bc41a6edde5b6ba62bf1a4ef1c931a87f321ad3
Ben McDonald <mcdonald@raptor.us.cray.com>
2021-12-01 11:46:04 -0600

    Fix parquet tests for multilocale runs

--- tests/parquet_test.py
+++ tests/parquet_test.py
@@ -1,6 +1,7 @@
 import glob, os
 from context import arkouda as ak
 from base_test import ArkoudaTest
+import numpy as np
 import pytest
 
 SIZE = 50
@@ -16,20 +17,26 @@ class ParquetTest(ArkoudaTest):
         # get the dset from the dictionary in multi-locale cases
         for f in glob.glob('pq_test*'):
             os.remove(f)
-            self.assertTrue((ak_arr ==  pq_arr).all())
+            a = ak_arr.to_ndarray().sort()
+            b = pq_arr.to_ndarray().sort()
+            self.assertTrue(a ==  b)
 
     def test_multi_file(self):
         adjusted_size = int(SIZE/NUMFILES)*NUMFILES
         test_arrs = []
+        elems = ak.randint(0, 2**32, adjusted_size)
+        per_arr = int(adjusted_size/NUMFILES)
         for i in range(NUMFILES):
-            test_arrs.append(ak.randint(0, 2**32, int(adjusted_size/NUMFILES)))
+            test_arrs.append(elems[(i*per_arr):(i*per_arr)+per_arr])
             test_arrs[i].save_parquet("pq_test" + str(i), "test-dset")
 
+        a = elems.to_ndarray()
+        a.sort()
         pq_arr = ak.read_parquet("pq_test*", "test-dset")
-        self.assertTrue(len(pq_arr) == adjusted_size)
+        b = pq_arr.to_ndarray()
+        b.sort()
 
-        for i in range(NUMFILES):
-            sz = len(test_arrs[i])
-            self.assertTrue((test_arrs[i] == pq_arr[(i*sz):(i*sz)+sz]).all())
+        self.assertTrue((a == b).all())
+
         for f in glob.glob('pq_test*'):
             os.remove(f)
