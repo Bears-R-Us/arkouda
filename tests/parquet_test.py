fe923b4bc5d7158cf9d8409c0ff1f5afefd2be1d
Ben McDonald <mcdobe100@gmail.com>
2021-12-01 16:46:03 -0800

    Fix parquet file suffix appending for sort order

--- tests/parquet_test.py
+++ tests/parquet_test.py
@@ -4,7 +4,7 @@ from base_test import ArkoudaTest
 import numpy as np
 import pytest
 
-SIZE = 50
+SIZE = 100
 NUMFILES = 5
 verbose = True
 
@@ -14,29 +14,25 @@ class ParquetTest(ArkoudaTest):
         ak_arr = ak.randint(0, 2**32, SIZE)
         ak_arr.save_parquet("pq_testcorrect", "my-dset")
         pq_arr = ak.read_parquet("pq_testcorrect*", "my-dset")
-        # get the dset from the dictionary in multi-locale cases
+        self.assertTrue((ak_arr ==  pq_arr).all())
+        
         for f in glob.glob('pq_test*'):
             os.remove(f)
-            a = ak_arr.to_ndarray().sort()
-            b = pq_arr.to_ndarray().sort()
-            self.assertTrue(a ==  b)
 
     def test_multi_file(self):
         adjusted_size = int(SIZE/NUMFILES)*NUMFILES
         test_arrs = []
         elems = ak.randint(0, 2**32, adjusted_size)
         per_arr = int(adjusted_size/NUMFILES)
+        print(elems)
         for i in range(NUMFILES):
             test_arrs.append(elems[(i*per_arr):(i*per_arr)+per_arr])
+            print(test_arrs[i])
             test_arrs[i].save_parquet("pq_test" + str(i), "test-dset")
 
-        a = elems.to_ndarray()
-        a.sort()
         pq_arr = ak.read_parquet("pq_test*", "test-dset")
-        b = pq_arr.to_ndarray()
-        b.sort()
 
-        self.assertTrue((a == b).all())
+        self.assertTrue((elems == pq_arr).all())
 
         for f in glob.glob('pq_test*'):
             os.remove(f)
