59f7474d374be5a5e2aaeeacc46d1a08f20404f5
Ethan-DeBandi99 <16845933+Ethan-DeBandi99@users.noreply.github.com>
2022-03-10 14:28:46 -0500

    Closes #1176 doc version num (#1198)
    * Updated quickstart doc to not hardcode server version. Updated output message to match current. Did not include *'s used in actual output.
    
    * Removed hard-coded version and replaced with empty string

--- pydoc/conf.py
+++ pydoc/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath('../benchmarks'))
 sys.path.insert(0, os.path.abspath('..'))
 
@@ -22,7 +23,7 @@ copyright = '2020, Michael Merrill and William Reus'
 author = 'Michael Merrill and William Reus'
 
 # The full version, including alpha/beta/rc tags
-release = '2020.07.07'
+release = ''
 
 
 # -- General configuration ---------------------------------------------------
