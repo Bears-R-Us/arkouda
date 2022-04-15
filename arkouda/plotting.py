2ad69b0258ca5b345944d64b6511cd0ac4e242c0
Ethan-DeBandi99 <16845933+Ethan-DeBandi99@users.noreply.github.com>
2022-04-15 06:40:54 -0400

    Readded docstring

--- arkouda/plotting.py
+++ arkouda/plotting.py
@@ -9,6 +9,37 @@ def log10(x):
     return basechange*log(x)
 
 def plot_dist(b, h, log=True, xlabel=None, newfig=True):
+    """
+    Plot the distribution and cumulative distribution of histogram Data
+
+    Parameters
+    ----------
+    b : np.ndarray
+        Bin edges
+    h : np.ndarray
+        Histogram data
+    log : bool
+        use log to scale y
+    xlabel: str
+        Label for the x axis of the graph
+    newfig: bool
+        Generate a new figure or not
+
+    Notes
+    -----
+    This function does not return or display the plot. A user must have matplotlib imported in addition to arkouda to
+    display plots. This could be updated to return the object or have a flag to show the resulting plots.
+    See Examples Below.
+
+    Examples
+    --------
+    >>> import arkouda as ak
+    >>> from matplotlib import pyplot as plt
+    >>> b, h = ak.histogram(ak.arange(10), 3)
+    >>> ak.plot_dist(b, h)
+    >>> # to show the plot
+    >>> plt.show()
+    """
     if newfig:
         plt.figure(figsize=(12, 5))
     plt.subplot(1, 2, 1)
