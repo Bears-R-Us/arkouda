78bc860c29c7062f6f188128efc56f74b1d447b4
Ethan-DeBandi99 <16845933+Ethan-DeBandi99@users.noreply.github.com>
2022-04-20 13:04:25 -0400

    Added pytest skipif to skip save_table test if parquet not installed.

--- tests/dataframe_test.py
+++ tests/dataframe_test.py
@@ -4,6 +4,7 @@ import string
 
 import os
 from shutil import rmtree
+import pytest
 
 from base_test import ArkoudaTest
 from context import arkouda as ak
@@ -426,6 +427,7 @@ class DataFrameTest(ArkoudaTest):
         df_copy.__setitem__('userID', ak.array([1, 2, 1, 3, 2, 1]))
         self.assertEqual(df.__repr__(), df_copy.__repr__())
 
+    @pytest.mark.skipif(not os.getenv('ARKOUDA_SERVER_PARQUET_SUPPORT'), reason="No parquet support")
     def test_save_table(self):
         i = list(range(3))
         c1 = [9, 7, 17]
