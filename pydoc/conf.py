7f4defc99619a6795bc078fc9c27a0f7de04457a
joshmarshall1 <josh@MSI.localdomain>
2022-04-27 10:49:41 -0400

    Adding comment to substitution variable

--- pydoc/conf.py
+++ pydoc/conf.py
@@ -65,4 +65,5 @@ html_theme = 'alabaster'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+# Add release substitution variable
 substitutions = [("|release|", release)]
