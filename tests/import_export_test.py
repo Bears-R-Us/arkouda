8c5c024a6f373f3e0d366e274f604d68f08f4859
Ethan-DeBandi99 <16845933+Ethan-DeBandi99@users.noreply.github.com>
2022-05-23 16:37:54 -0400

    Updated tests to reflect that object is now returned by default.

--- tests/import_export_test.py
+++ tests/import_export_test.py
@@ -33,29 +33,24 @@ class DataFrameTest(ArkoudaTest):
 
         pddf = self.build_pandas_dataframe()
         pddf.to_hdf(f"{f_base}/table.h5", "dataframe", format="Table", mode="w")
-        akdf = ak.import_data(f"{f_base}/table.h5", write_file=f"{f_base}/ak_table.h5",
-                              return_obj=True)
+        akdf = ak.import_data(f"{f_base}/table.h5", write_file=f"{f_base}/ak_table.h5")
         self.assertTrue(len(glob.glob(f"{f_base}/ak_table_*.h5")) == locales)
         self.assertTrue(pddf.equals(akdf.to_pandas()))
 
         pddf.to_hdf(f"{f_base}/table_columns.h5", "dataframe", format="Table", data_columns=True, mode="w")
-        akdf = ak.import_data(f"{f_base}/table_columns.h5", write_file=f"{f_base}/ak_table_columns.h5",
-                              return_obj=True)
+        akdf = ak.import_data(f"{f_base}/table_columns.h5", write_file=f"{f_base}/ak_table_columns.h5")
         self.assertTrue(len(glob.glob(f"{f_base}/ak_table_columns_*.h5")) == locales)
         self.assertTrue(pddf.equals(akdf.to_pandas()))
 
         pddf.to_hdf(f"{f_base}/fixed.h5", "dataframe", format="fixed", data_columns=True, mode="w")
-        akdf = ak.import_data(f"{f_base}/fixed.h5", write_file=f"{f_base}/ak_fixed.h5",
-                              return_obj=True)
+        akdf = ak.import_data(f"{f_base}/fixed.h5", write_file=f"{f_base}/ak_fixed.h5")
         self.assertTrue(len(glob.glob(f"{f_base}/ak_fixed_*.h5")) == locales)
         self.assertTrue(pddf.equals(akdf.to_pandas()))
 
         with self.assertRaises(FileNotFoundError):
-            akdf = ak.import_data(f"{f_base}/foo.h5", write_file=f"{f_base}/ak_fixed.h5",
-                                  return_obj=True)
+            akdf = ak.import_data(f"{f_base}/foo.h5", write_file=f"{f_base}/ak_fixed.h5")
         with self.assertRaises(RuntimeError):
-            akdf = ak.import_data(f"{f_base}/*.h5", write_file=f"{f_base}/ak_fixed.h5",
-                                  return_obj=True)
+            akdf = ak.import_data(f"{f_base}/*.h5", write_file=f"{f_base}/ak_fixed.h5")
 
         # clean up test files
         rmtree(f_base)
@@ -68,14 +63,12 @@ class DataFrameTest(ArkoudaTest):
         akdf = self.build_arkouda_dataframe()
         akdf.save_table(f"{f_base}/ak_write")
 
-        pddf = ak.export(f"{f_base}/ak_write", write_file=f"{f_base}/pd_from_ak.h5",
-                         return_obj=True, index=True)
+        pddf = ak.export(f"{f_base}/ak_write", write_file=f"{f_base}/pd_from_ak.h5", index=True)
         self.assertTrue(len(glob.glob(f"{f_base}/pd_from_ak.h5")) == 1)
         self.assertTrue(pddf.equals(akdf.to_pandas()))
 
         with self.assertRaises(RuntimeError):
-            pddf = ak.export(f"{f_base}/foo.h5", write_file=f"{f_base}/pd_from_ak.h5",
-                             return_obj=True, index=True)
+            pddf = ak.export(f"{f_base}/foo.h5", write_file=f"{f_base}/pd_from_ak.h5", index=True)
 
         # clean up test files
         rmtree(f_base)
@@ -89,9 +82,7 @@ class DataFrameTest(ArkoudaTest):
 
         pddf = self.build_pandas_dataframe()
         pddf.to_parquet(f"{f_base}/table.parquet")
-        akdf = ak.import_data(f"{f_base}/table.parquet", write_file=f"{f_base}/ak_table.parquet",
-                              return_obj=True)
-        ### TODO - fix locale crap
+        akdf = ak.import_data(f"{f_base}/table.parquet", write_file=f"{f_base}/ak_table.parquet")
         self.assertTrue(len(glob.glob(f"{f_base}/ak_table_*.parquet")) == locales)
         self.assertTrue(pddf.equals(akdf.to_pandas()))
 
@@ -108,15 +99,13 @@ class DataFrameTest(ArkoudaTest):
         akdf.save_table(f"{f_base}/ak_write", file_format="Parquet")
         print(akdf.__repr__())
 
-        pddf = ak.export(f"{f_base}/ak_write", write_file=f"{f_base}/pd_from_ak.parquet",
-                         return_obj=True, index=True)
+        pddf = ak.export(f"{f_base}/ak_write", write_file=f"{f_base}/pd_from_ak.parquet", index=True)
         print(pddf)
         self.assertTrue(len(glob.glob(f"{f_base}/pd_from_ak.parquet")) == 1)
         self.assertTrue(pddf.equals(akdf.to_pandas()))
 
         with self.assertRaises(RuntimeError):
-            pddf = ak.export(f"{f_base}/foo.h5", write_file=f"{f_base}/pd_from_ak.h5",
-                             return_obj=True, index=True)
+            pddf = ak.export(f"{f_base}/foo.h5", write_file=f"{f_base}/pd_from_ak.h5", index=True)
 
         # clean up test files
         rmtree(f_base)
