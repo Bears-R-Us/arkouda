f0f9cd9b5fddb1a0b89959808d31ff9a14139dc7
Ethan-DeBandi99 <16845933+Ethan-DeBandi99@users.noreply.github.com>
2022-05-31 14:57:55 -0400

    str-locality black, isort, flake8. Some errors ignored for benchmark to run properly. ignoring lambda error.

--- benchmarks/str-locality.py
+++ benchmarks/str-locality.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
 
-import time
 import argparse
+import time
+
 import arkouda as ak
 
+
 def generate_data(N, seed):
-    prefix = ak.random_strings_uniform(minlen=1, maxlen=16, size=N, seed=seed, characters='numeric')
+    prefix = ak.random_strings_uniform(minlen=1, maxlen=16, size=N, seed=seed, characters="numeric")
     if seed is not None:
         seed += 1
-    suffix = ak.random_strings_uniform(minlen=1, maxlen=16, size=N, seed=seed, characters='numeric')
-    random_strings = prefix.stick(suffix, delimiter='.')
+    suffix = ak.random_strings_uniform(minlen=1, maxlen=16, size=N, seed=seed, characters="numeric")
+    random_strings = prefix.stick(suffix, delimiter=".")
     perm = ak.argsort(random_strings.get_lengths())
     sorted_strings = random_strings[perm]
     return random_strings, sorted_strings, perm
+
 
 def time_all_ops(N_per_locale, trials, seed, correctnessOnly):
     if correctnessOnly:
@@ -50,12 +53,12 @@ def time_all_ops(N_per_locale, trials, seed, correctnessOnly):
 
     # Hash
     op = lambda x: x.hash()
-    ans1, ans2 = time_op(op, 'Hashing')
+    ans1, ans2 = time_op(op, "Hashing")
     for i in range(2):
         assert (ans1[i][perm] == ans2[i]).all()
 
     # Substring search
-    op = lambda x: x.contains(r'\d{3,5}\.\d{5,8}', regex=True)
+    op = lambda x: x.contains(r"\d{3,5}\.\d{5,8}", regex=True)
     ans1, ans2 = time_op(op, "Regex searching")
     assert (ans1[perm] == ans2).all()
 
@@ -69,19 +72,35 @@ def time_all_ops(N_per_locale, trials, seed, correctnessOnly):
     ans1, ans2 = time_op(op, "Comparing to scalar")
     assert (ans1[perm] == ans2).all()
 
+
 def create_parser():
-    parser = argparse.ArgumentParser(description="Measure the performance of various string operations on strings with good locality (random) and poor locality (sorted).")
-    parser.add_argument('hostname', help='Hostname of arkouda server')
-    parser.add_argument('port', type=int, help='Port of arkouda server')
-    parser.add_argument('-n', '--size', type=int, default=10**7, help='Problem size: Number of Strings to search')
-    parser.add_argument('-t', '--trials', type=int, default=1, help='Number of times to run the benchmark')
-    parser.add_argument('--correctness-only', default=False, action='store_true', help='Only check correctness, not performance.')
-    parser.add_argument('-s', '--seed', default=None, type=int, help='Value to initialize random number generator')
+    parser = argparse.ArgumentParser(
+        description="Measure the performance of various string operations on "
+                    "strings with good locality (random) and poor locality (sorted)."
+    )
+    parser.add_argument("hostname", help="Hostname of arkouda server")
+    parser.add_argument("port", type=int, help="Port of arkouda server")
+    parser.add_argument(
+        "-n", "--size", type=int, default=10**7, help="Problem size: Number of Strings to search"
+    )
+    parser.add_argument(
+        "-t", "--trials", type=int, default=1, help="Number of times to run the benchmark"
+    )
+    parser.add_argument(
+        "--correctness-only",
+        default=False,
+        action="store_true",
+        help="Only check correctness, not performance.",
+    )
+    parser.add_argument(
+        "-s", "--seed", default=None, type=int, help="Value to initialize random number generator"
+    )
     return parser
 
 
 if __name__ == "__main__":
     import sys
+
     parser = create_parser()
     args = parser.parse_args()
     ak.verbose = False
