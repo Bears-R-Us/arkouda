a73b91dc97d8ab5085bf3d3d240f279f02781ed3
Ethan-DeBandi99 <16845933+Ethan-DeBandi99@users.noreply.github.com>
2022-05-31 14:07:18 -0400

    dataframe black, isort, flake8

--- benchmarks/dataframe.py
+++ benchmarks/dataframe.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 
-import time, argparse
+import argparse
+import time
+
 import numpy as np
 import pandas as pd
 
 import arkouda as ak
 
-OPS = ['_get_head_tail_server', '_get_head_tail']
-TYPES = ('int64', 'uint64',)
+OPS = ["_get_head_tail_server", "_get_head_tail"]
+TYPES = (
+    "int64",
+    "uint64",
+)
 
 
 def generate_dataframe(N, seed):
@@ -23,11 +28,13 @@ def generate_dataframe(N, seed):
             str_arr = ak.random_strings_uniform(minlen=5, maxlen=6, size=N, seed=seed)
             df_dict[key] = ak.Categorical(str_arr)
         elif d == ak.pdarray:
-            df_dict[key] = ak.array(np.random.randint(0, 2 ** 32, N))
+            df_dict[key] = ak.array(np.random.randint(0, 2**32, N))
         elif d == ak.Strings:
             df_dict[key] = ak.random_strings_uniform(minlen=5, maxlen=6, size=N, seed=seed)
         elif d == ak.SegArray:
-            df_dict[key] = ak.SegArray(ak.arange(0, N*5, 5), ak.array(np.random.randint(0, 2 ** 32, N*5)))
+            df_dict[key] = ak.SegArray(
+                ak.arange(0, N * 5, 5), ak.array(np.random.randint(0, 2**32, N * 5))
+            )
     return ak.DataFrame(df_dict)
 
 
@@ -73,7 +80,7 @@ def time_ak_df_display(N_per_locale, trials, seed):
     for op, t in tavg.items():
         print("  {} Average time = {:.4f} sec".format(op, t))
         bytes_per_sec = nbytes / t
-        print("  {} Average rate = {:.2f} GiB/sec".format(op, bytes_per_sec / 2 ** 30))
+        print("  {} Average rate = {:.2f} GiB/sec".format(op, bytes_per_sec / 2**30))
 
 
 def check_correctness(N_per_locale, seed):
@@ -85,23 +92,36 @@ def check_correctness(N_per_locale, seed):
     pd.set_option("display.min_rows", 10)
     pd.set_option("display.max_columns", 20)
 
-    printdf = df._get_head_tail_server() # measure the pandas df returned
+    printdf = df._get_head_tail_server()  # measure the pandas df returned
     # Mainly want to verify shape for the print
-    assert(printdf.shape[0] == 101)
-    assert(printdf.shape[1] == 20)
+    assert printdf.shape[0] == 101
+    assert printdf.shape[1] == 20
 
 
 def create_parser():
-    parser = argparse.ArgumentParser(description="Run the dataframe display benchmarks: "
-                                                 "_get_head_tail, _get_head_tail_server")
-    parser.add_argument('hostname', help='Hostname of arkouda server')
-    parser.add_argument('port', type=int, help='Port of arkouda server')
-    parser.add_argument('-n', '--size', type=int, default=10**4, help='Problem size: length of columns in dataframe.')
-    parser.add_argument('-t', '--trials', type=int, default=1, help='Number of times to run the benchmark')
-    parser.add_argument('-d', '--dtype', default='int64', help='Dtype of array ({})'.format(', '.join(TYPES)))
-    parser.add_argument('--correctness-only', default=False, action='store_true',
-                        help='Only check correctness, not performance.')
-    parser.add_argument('-s', '--seed', default=None, type=int, help='Value to initialize random number generator')
+    parser = argparse.ArgumentParser(
+        description="Run the dataframe display benchmarks: " "_get_head_tail, _get_head_tail_server"
+    )
+    parser.add_argument("hostname", help="Hostname of arkouda server")
+    parser.add_argument("port", type=int, help="Port of arkouda server")
+    parser.add_argument(
+        "-n", "--size", type=int, default=10**4, help="Problem size: length of columns in dataframe."
+    )
+    parser.add_argument(
+        "-t", "--trials", type=int, default=1, help="Number of times to run the benchmark"
+    )
+    parser.add_argument(
+        "-d", "--dtype", default="int64", help="Dtype of array ({})".format(", ".join(TYPES))
+    )
+    parser.add_argument(
+        "--correctness-only",
+        default=False,
+        action="store_true",
+        help="Only check correctness, not performance.",
+    )
+    parser.add_argument(
+        "-s", "--seed", default=None, type=int, help="Value to initialize random number generator"
+    )
     return parser
 
 
@@ -111,7 +131,7 @@ if __name__ == "__main__":
     parser = create_parser()
     args = parser.parse_args()
     if args.dtype not in TYPES:
-        raise ValueError("Dtype must be {}, not {}".format('/'.join(TYPES), args.dtype))
+        raise ValueError("Dtype must be {}, not {}".format("/".join(TYPES), args.dtype))
 
     ak.verbose = False
     ak.connect(args.hostname, args.port)
