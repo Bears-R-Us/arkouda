3b5ba4f2813f0eb3bfe82230225395eac7fc65c4
Ethan-DeBandi99 <16845933+Ethan-DeBandi99@users.noreply.github.com>
2022-05-31 11:17:32 -0400

    coargsort_test.py black, isort, flake8

--- tests/coargsort_test.py
+++ tests/coargsort_test.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
-import time, argparse
-import numpy as np
-from context import arkouda as ak 
+import argparse
+
 from base_test import ArkoudaTest
+from context import arkouda as ak
+
 
 def check_integral(N, algo, dtype):
     z = ak.zeros(N, dtype=dtype)
@@ -32,7 +33,6 @@ def check_integral(N, algo, dtype):
     perm = ak.coargsort([z, z, z, d2], algo)
     assert ak.is_sorted(d2[perm])
 
-
     a4 = ak.randint(0, 2**32, N)
     b4 = ak.randint(0, 2**32, N)
     n4 = ak.randint(-(2**31), 2**31, N)
@@ -49,11 +49,9 @@ def check_integral(N, algo, dtype):
     perm = ak.coargsort([b4, a4], algo)
     assert ak.is_sorted(b4[perm])
 
-
     a8 = ak.randint(0, 2**64, N)
     b8 = ak.randint(0, 2**64, N)
     n8 = ak.randint(-(2**63), 2**64, N)
- 
 
     perm = ak.coargsort([a8], algo)
     assert ak.is_sorted(a8[perm])
@@ -70,6 +68,7 @@ def check_integral(N, algo, dtype):
     for p in all_perm:
         perm = ak.coargsort(p, algo)
         assert ak.is_sorted(p[0][perm])
+
 
 def check_float(N, algo):
     a = ak.randint(0, 1, N, dtype=ak.float64)
@@ -91,6 +90,7 @@ def check_float(N, algo):
     perm = ak.coargsort([z, n], algo)
     assert ak.is_sorted(n[perm])
 
+
 def check_int_uint_float(N, algo):
     f = ak.randint(0, 2**63, N, dtype=ak.float64)
     u = ak.randint(0, 2**63, N, dtype=ak.uint64)
@@ -105,10 +105,12 @@ def check_int_uint_float(N, algo):
     perm = ak.coargsort([i, f, u], algo)
     assert ak.is_sorted(i[perm])
 
+
 def check_large(N, algo):
-    l = [ak.randint(0, 2**63, N) for _ in range(10)]
-    perm = ak.coargsort(l, algo)
-    assert ak.is_sorted(l[0][perm])
+    lg = [ak.randint(0, 2**63, N) for _ in range(10)]
+    perm = ak.coargsort(lg, algo)
+    assert ak.is_sorted(lg[0][perm])
+
 
 def check_coargsort(N_per_locale):
     print(">>> arkouda coargsort")
@@ -123,8 +125,8 @@ def check_coargsort(N_per_locale):
         check_int_uint_float(N, algo)
         check_large(N, algo)
 
-class CoargsortTest(ArkoudaTest):
 
+class CoargsortTest(ArkoudaTest):
     def test_int(self):
         for algo in ak.SortingAlgorithm:
             check_integral(10**3, algo, ak.int64)
@@ -144,7 +146,7 @@ class CoargsortTest(ArkoudaTest):
     def test_large(self):
         for algo in ak.SortingAlgorithm:
             check_large(10**3, algo)
-        
+
     def test_error_handling(self):
         ones = ak.ones(100)
         short_ones = ak.ones(10)
@@ -155,13 +157,14 @@ class CoargsortTest(ArkoudaTest):
 
         for algo in ak.SortingAlgorithm:
             with self.assertRaises(TypeError):
-                ak.coargsort([list(range(0,10)), [0]], algo)
+                ak.coargsort([list(range(0, 10)), [0]], algo)
 
     def test_coargsort_categorical(self):
-        string = ak.array(['a', 'b', 'a', 'b', 'c'])
+        string = ak.array(["a", "b", "a", "b", "c"])
         cat = ak.Categorical(string)
-        cat_from_codes = ak.Categorical.from_codes(codes=ak.array([0, 1, 0, 1, 2]),
-                                                   categories=ak.array(['a', 'b', 'c']))
+        cat_from_codes = ak.Categorical.from_codes(
+            codes=ak.array([0, 1, 0, 1, 2]), categories=ak.array(["a", "b", "c"])
+        )
         for algo in ak.SortingAlgorithm:
             str_perm = ak.coargsort([string], algo)
             str_sorted = string[str_perm].to_ndarray()
@@ -175,7 +178,7 @@ class CoargsortTest(ArkoudaTest):
             # coargsort sorts using codes, the order isn't guaranteed, only grouping
             from_codes_perm = ak.coargsort([cat_from_codes], algo)
             from_codes_sorted = cat_from_codes[from_codes_perm].to_ndarray()
-            self.assertTrue((['a', 'a', 'b', 'b', 'c'] == from_codes_sorted).all())
+            self.assertTrue((["a", "a", "b", "b", "c"] == from_codes_sorted).all())
 
             # coargsort on 2 categoricals (one from_codes)
             cat_perm = ak.coargsort([cat, cat_from_codes], algo)
@@ -190,13 +193,17 @@ class CoargsortTest(ArkoudaTest):
 
 def create_parser():
     parser = argparse.ArgumentParser(description="Check coargsort correctness.")
-    parser.add_argument('hostname', help='Hostname of arkouda server')
-    parser.add_argument('port', type=int, help='Port of arkouda server')
-    parser.add_argument('-n', '--size', type=int, default=10**3, help='Problem size: length of array to argsort')
+    parser.add_argument("hostname", help="Hostname of arkouda server")
+    parser.add_argument("port", type=int, help="Port of arkouda server")
+    parser.add_argument(
+        "-n", "--size", type=int, default=10**3, help="Problem size: length of array to argsort"
+    )
     return parser
+
 
 if __name__ == "__main__":
     import sys
+
     parser = create_parser()
     args = parser.parse_args()
     ak.verbose = False
