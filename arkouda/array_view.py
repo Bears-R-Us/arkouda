2bf7a217c5a3c5d6f53c762a1347b8f6967a3e76
Pierce Hayes <pierce314159@users.noreply.github.com>
2022-06-01 17:31:43 -0400

    `array_view.py` black, isort, flake8

--- arkouda/array_view.py
+++ arkouda/array_view.py
@@ -1,20 +1,24 @@
-from arkouda.pdarrayclass import pdarray, parse_single_value, create_pdarray
-from arkouda.pdarraycreation import array, arange, zeros, ones, full
-from arkouda.pdarraysetops import concatenate
-from arkouda.numeric import cumprod, where, cast as akcast
-import numpy as np  # type: ignore
-from arkouda.client import generic_msg
-from arkouda.dtypes import translate_np_dtype, resolve_scalar_dtype
-from enum import Enum
 import json
+from enum import Enum
 
-OrderType = Enum('OrderType', ['ROW_MAJOR', 'COLUMN_MAJOR'])
+import numpy as np  # type: ignore
+
+from arkouda.client import generic_msg
+from arkouda.dtypes import resolve_scalar_dtype, translate_np_dtype
+from arkouda.numeric import cast as akcast
+from arkouda.numeric import cumprod, where
+from arkouda.pdarrayclass import create_pdarray, parse_single_value, pdarray
+from arkouda.pdarraycreation import arange, array, ones, zeros
+from arkouda.pdarraysetops import concatenate
+
+OrderType = Enum("OrderType", ["ROW_MAJOR", "COLUMN_MAJOR"])
 
 
 class ArrayView:
     """
     A multi-dimensional view of a pdarray. Arkouda ``ArraryView`` behaves similarly to numpy's ndarray.
-    The base pdarray is stored in 1-dimension but can be indexed and treated logically as if it were multi-dimensional
+    The base pdarray is stored in 1-dimension but can be indexed and treated logically
+    as if it were multi-dimensional
 
     Attributes
     ----------
@@ -35,7 +39,8 @@ class ArrayView:
         By default or if 'C'/'row_major', read and write data in row_major order
         If 'F'/'column_major', read and write data in column_major order
     """
-    def __init__(self, base: pdarray, shape, order='row_major'):
+
+    def __init__(self, base: pdarray, shape, order="row_major"):
         self.objtype = type(self).__name__
         self.shape = array(shape)
         if not isinstance(self.shape, pdarray):
@@ -47,9 +52,9 @@ class ArrayView:
         self.dtype = base.dtype
         self.ndim = self.shape.size
         self.itemsize = self.base.itemsize
-        if order.upper() in {'C', 'ROW_MAJOR'}:
+        if order.upper() in {"C", "ROW_MAJOR"}:
             self.order = OrderType.ROW_MAJOR
-        elif order.upper() in {'F', 'COLUMN_MAJOR'}:
+        elif order.upper() in {"F", "COLUMN_MAJOR"}:
             self.order = OrderType.COLUMN_MAJOR
         else:
             raise ValueError(f"cannot traverse with order={order}")
@@ -60,31 +65,37 @@ class ArrayView:
             self._dim_prod = zeros(self.shape.size, self.dtype)
         else:
             # cache dim_prod to avoid recalculation, reverse if row_major
-            self._dim_prod = cumprod(self.shape)//self.shape if self.order is OrderType.COLUMN_MAJOR else cumprod(self._reverse_shape)//self._reverse_shape
+            self._dim_prod = (
+                cumprod(self.shape) // self.shape
+                if self.order is OrderType.COLUMN_MAJOR
+                else cumprod(self._reverse_shape) // self._reverse_shape
+            )
 
     def __len__(self):
         return self.size
 
     def __repr__(self):
         from arkouda.client import pdarrayIterThresh
+
         if self.size <= pdarrayIterThresh:
             return self.to_ndarray().__repr__()
         else:
-            edge_items = np.get_printoptions()['edgeitems']
+            edge_items = np.get_printoptions()["edgeitems"]
             vals = [f"'{self.base[i]}'" for i in range(edge_items)]
-            vals.append('... ')
-            vals.extend([f"'{self.base[i]}'" for i in range(self.size-edge_items, self.size)])
+            vals.append("... ")
+            vals.extend([f"'{self.base[i]}'" for i in range(self.size - edge_items, self.size)])
         return f"array([{', '.join(vals)}]), shape {self.shape}"
 
     def __str__(self):
         from arkouda.client import pdarrayIterThresh
+
         if self.size <= pdarrayIterThresh:
             return self.to_ndarray().__str__()
         else:
-            edge_items = np.get_printoptions()['edgeitems']
+            edge_items = np.get_printoptions()["edgeitems"]
             vals = [f"'{self.base[i]}'" for i in range(edge_items)]
-            vals.append('... ')
-            vals.extend([f"'{self.base[i]}'" for i in range(self.size-edge_items, self.size)])
+            vals.append("... ")
+            vals.extend([f"'{self.base[i]}'" for i in range(self.size - edge_items, self.size)])
         return f"[{', '.join(vals)}], shape {self.shape}"
 
     def __getitem__(self, key):
@@ -93,16 +104,20 @@ class ArrayView:
         elif isinstance(key, tuple):
             key = list(key)
         if len(key) > self.ndim:
-            raise IndexError(f"too many indices for array: array is {self.ndim}-dimensional, but {len(key)} were indexed")
+            raise IndexError(
+                f"too many indices for array: array is {self.ndim}-dimensional, "
+                f"but {len(key)} were indexed"
+            )
         if len(key) < self.ndim:
             # append self.ndim-len(key) many ':'s to fill in the missing dimensions
             for i in range(self.ndim - len(key)):
                 key.append(slice(None, None, None))
         try:
             # attempt to convert to a pdarray (allows for view[0,2,1] instead of view[ak.array([0,2,1])]
-            # but pass on RuntimeError to allow things like view[0,:,[True,False,True]] to be correctly handled
+            # but pass on RuntimeError to allow things like
+            # view[0,:,[True,False,True]] to be correctly handled
             key = array(key)
-        except:  # bare except is bad practice but we want to pass on any errors/warnings
+        except (RuntimeError, TypeError, ValueError, DeprecationWarning):
             pass
         if isinstance(key, pdarray):
             kind, _ = translate_np_dtype(key.dtype)
@@ -111,21 +126,30 @@ class ArrayView:
             if kind == "bool":
                 if key.all():
                     # every dimension is True, so return this arrayview with shape = [1, self.shape]
-                    return self.base.reshape(concatenate([ones(1, dtype=self.dtype), self.shape]), order=self.order.name)
+                    return self.base.reshape(
+                        concatenate([ones(1, dtype=self.dtype), self.shape]), order=self.order.name
+                    )
                 else:
-                    # at least one dimension is False, so return empty arrayview with shape = [0, self.shape]
-                    return array([], dtype=self.dtype).reshape(concatenate([zeros(1, dtype=self.dtype), self.shape]), order=self.order.name)
+                    # at least one dimension is False,
+                    # so return empty arrayview with shape = [0, self.shape]
+                    return array([], dtype=self.dtype).reshape(
+                        concatenate([zeros(1, dtype=self.dtype), self.shape]), order=self.order.name
+                    )
             # Interpret negative key as offset from end of array
             key = where(key < 0, akcast(key + self.shape, kind), key)
             # Capture the indices which are still out of bounds
             out_of_bounds = (key < 0) | (self.shape <= key)
             if out_of_bounds.any():
                 out = arange(key.size)[out_of_bounds][0]
-                raise IndexError(f"index {key[out]} is out of bounds for axis {out} with size {self.shape[out]}")
+                raise IndexError(
+                    f"index {key[out]} is out of bounds for axis {out} with size {self.shape[out]}"
+                )
             coords = key if self.order is OrderType.COLUMN_MAJOR else key[::-1]
-            repMsg = generic_msg(cmd="arrayViewIntIndex", args=f"{self.base.name} {self._dim_prod.name} {coords.name}")
+            repMsg = generic_msg(
+                cmd="arrayViewIntIndex", args=f"{self.base.name} {self._dim_prod.name} {coords.name}"
+            )
             fields = repMsg.split()
-            return parse_single_value(' '.join(fields[1:]))
+            return parse_single_value(" ".join(fields[1:]))
         elif isinstance(key, list):
             indices = []
             reshape_dim_list = []
@@ -133,21 +157,24 @@ class ArrayView:
             key = key if self.order is OrderType.COLUMN_MAJOR else key[::-1]
             for i in range(len(key)):
                 x = key[i]
-                if np.isscalar(x) and (resolve_scalar_dtype(x) == 'int64' or 'uint64'):
+                if np.isscalar(x) and (resolve_scalar_dtype(x) == "int64" or "uint64"):
                     orig_key = x
                     if x < 0:
                         # Interpret negative key as offset from end of array
                         x += self._reverse_shape[i]
                     if 0 <= x < self._reverse_shape[i]:
-                        indices.append('int')
+                        indices.append("int")
                         # have to cast to int because JSON doesn't recognize numpy dtypes
                         indices.append(json.dumps(int(x)))
                         index_dim_list.append(1)
                     else:
-                        raise IndexError(f"index {orig_key} is out of bounds for axis {i} with size {self._reverse_shape[i]}")
+                        raise IndexError(
+                            f"index {orig_key} is out of bounds for axis {i} "
+                            f"with size {self._reverse_shape[i]}"
+                        )
                 elif isinstance(x, slice):
                     (start, stop, stride) = x.indices(self._reverse_shape[i])
-                    indices.append('slice')
+                    indices.append("slice")
                     indices.append(json.dumps((start, stop, stride)))
                     slice_size = len(range(*(start, stop, stride)))
                     index_dim_list.append(slice_size)
@@ -168,12 +195,15 @@ class ArrayView:
                 else:
                     raise TypeError(f"Unhandled key type: {x} ({type(x)})")
             index_dim = array(index_dim_list)
-            repMsg = generic_msg(cmd="arrayViewMixedIndex", args="{} {} {} {} {}".format(self.base.name,
-                                                                                         index_dim.name,
-                                                                                         self.ndim,
-                                                                                         self._dim_prod.name,
-                                                                                         json.dumps(indices)))
-            reshape_dim = reshape_dim_list if self.order is OrderType.COLUMN_MAJOR else reshape_dim_list[::-1]
+            repMsg = generic_msg(
+                cmd="arrayViewMixedIndex",
+                args="{} {} {} {} {}".format(
+                    self.base.name, index_dim.name, self.ndim, self._dim_prod.name, json.dumps(indices)
+                ),
+            )
+            reshape_dim = (
+                reshape_dim_list if self.order is OrderType.COLUMN_MAJOR else reshape_dim_list[::-1]
+            )
             return create_pdarray(repMsg).reshape(reshape_dim, order=self.order.name)
         else:
             raise TypeError(f"Unhandled key type: {key} ({type(key)})")
@@ -184,16 +214,20 @@ class ArrayView:
         elif isinstance(key, tuple):
             key = list(key)
         if len(key) > self.ndim:
-            raise IndexError(f"too many indices for array: array is {self.ndim}-dimensional, but {len(key)} were indexed")
+            raise IndexError(
+                f"too many indices for array: array is {self.ndim}-dimensional, "
+                f"but {len(key)} were indexed"
+            )
         if len(key) < self.ndim:
             # append self.ndim-len(key) many ':'s to fill in the missing dimensions
             for i in range(self.ndim - len(key)):
                 key.append(slice(None, None, None))
         try:
             # attempt to convert to a pdarray (allows for view[0,2,1] instead of view[ak.array([0,2,1])]
-            # but pass on RuntimeError to allow things like view[0,:,[True,False,True]] to be correctly handled
+            # but pass on RuntimeError to allow things like
+            # view[0,:,[True,False,True]] to be correctly handled
             key = array(key)
-        except:  # bare except is bad practice but we want to pass on any errors/warnings
+        except (RuntimeError, TypeError, ValueError, DeprecationWarning):
             pass
         if isinstance(key, pdarray):
             kind, _ = translate_np_dtype(key.dtype)
@@ -211,15 +245,22 @@ class ArrayView:
                 out_of_bounds = (key < 0) | (self.shape <= key)
                 if out_of_bounds.any():
                     out = arange(key.size)[out_of_bounds][0]
-                    raise IndexError(f"index {key[out]} is out of bounds for axis {out} with size {self.shape[out]}")
+                    raise IndexError(
+                        f"index {key[out]} is out of bounds for axis {out} with size {self.shape[out]}"
+                    )
                 coords = key if self.order is OrderType.COLUMN_MAJOR else key[::-1]
-                generic_msg(cmd="arrayViewIntIndexAssign", args="{} {} {} {} {}".format(self.base.name,
-                                                                                        self.dtype.name,
-                                                                                        self._dim_prod.name,
-                                                                                        coords.name,
-                                                                                        self.base.format_other(value)))
+                generic_msg(
+                    cmd="arrayViewIntIndexAssign",
+                    args="{} {} {} {} {}".format(
+                        self.base.name,
+                        self.dtype.name,
+                        self._dim_prod.name,
+                        coords.name,
+                        self.base.format_other(value),
+                    ),
+                )
         elif isinstance(key, list):
-            raise NotImplementedError(f"Setting via slicing and advanced indexing is not yet supported")
+            raise NotImplementedError("Setting via slicing and advanced indexing is not yet supported")
         else:
             raise TypeError(f"Unhandled key type: {key} ({type(key)})")
 
@@ -266,4 +307,4 @@ class ArrayView:
         if self.order is OrderType.ROW_MAJOR:
             return self.base.to_ndarray().reshape(self.shape.to_ndarray())
         else:
-            return self.base.to_ndarray().reshape(self.shape.to_ndarray(), order='F')
+            return self.base.to_ndarray().reshape(self.shape.to_ndarray(), order="F")
