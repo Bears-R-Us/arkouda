3b07fe54be384ff14c3c4492d09c2d59cd72f09e
Pierce Hayes <pierce314159@users.noreply.github.com>
2022-06-01 17:36:03 -0400

    `io_util.py` black, isort, flake8

--- arkouda/io_util.py
+++ arkouda/io_util.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 from typing import Any, Dict, Mapping
 
-def get_directory(path : str) -> Path:
-    '''
+
+def get_directory(path: str) -> Path:
+    """
     Creates the directory if it does not exist and then
     returns the corresponding Path object
 
@@ -10,25 +11,26 @@ def get_directory(path : str) -> Path:
     ----------
     path : str
         The path to the directory
-    
+
     Returns
     -------
     str
         Path object corresponding to the directory
-        
+
     Raises
     ------
     ValueError
         Raised if there's an error in reading an
         existing directory or creating a new one
-    '''
+    """
     try:
         Path(path).mkdir(parents=True, exist_ok=True)
-        return Path(path) 
+        return Path(path)
     except Exception as e:
         raise ValueError(e)
 
-def write_line_to_file(path : str, line : str) -> None:
+
+def write_line_to_file(path: str, line: str) -> None:
     """
     Writes a line to the requested file. Note: if the file
     does not exist, the file is created first and then
@@ -48,18 +50,18 @@ def write_line_to_file(path : str, line : str) -> None:
     Raises
     ------
     UnsupportedOption
-        Raised if there's an error in creating or 
+        Raised if there's an error in creating or
         writing to the file
     """
-    with open(path, 'a') as f:
-        f.write(''.join([line,'\n']))
+    with open(path, "a") as f:
+        f.write("".join([line, "\n"]))
 
-def delimited_file_to_dict(path : str, 
-                      delimiter : str=',') -> Dict[str,str]: 
+
+def delimited_file_to_dict(path: str, delimiter: str = ",") -> Dict[str, str]:
     """
-    Returns a dictionary populated by lines from a file where 
+    Returns a dictionary populated by lines from a file where
     the first delimited element of each line is the key and
-    the second delimited element is the value. 
+    the second delimited element is the value.
 
     Parameters
     ----------
@@ -76,25 +78,25 @@ def delimited_file_to_dict(path : str,
 
     Raises
     ------
-    UnsupportedOperation 
+    UnsupportedOperation
         Raised if there's an error in reading the file
     """
-    values : Dict[str,str] = {}
-    
-    with open(path,'a+') as f:
+    values: Dict[str, str] = {}
+
+    with open(path, "a+") as f:
         f.seek(0)
         for line in f:
             line = line.rstrip()
-            key,value = line.split(delimiter)
+            key, value = line.split(delimiter)
             values[key] = value
     return values
 
-def dict_to_delimited_file(path : str, values : Mapping[Any,Any],
-                      delimiter : str=',') -> None:
+
+def dict_to_delimited_file(path: str, values: Mapping[Any, Any], delimiter: str = ",") -> None:
     """
     Writes a dictionary to delimited lines in a file where
     the first delimited element of each line is the dict key
-    and the second delimited element is the dict value. If the 
+    and the second delimited element is the dict value. If the
     file does not exist, it is created and then written to.
 
     Parameters
@@ -103,23 +105,22 @@ def dict_to_delimited_file(path : str, values : Mapping[Any,Any],
         Path to the file
     delimiter
         Delimiter separating key and value
-        
+
     Returns
     -------
     None
-    
+
     Raises
     ------
-    OError 
-        Raised if there's an error opening or writing to the 
+    OError
+        Raised if there's an error opening or writing to the
         specified file
-    ValueError 
+    ValueError
         Raised if the delimiter is not supported
     """
-    if ',' == delimiter:
-        with open(path, 'w+') as f:
-            for key,value in values.items():
-                f.write('{},{}\n'.format(key,value))
+    if "," == delimiter:
+        with open(path, "w+") as f:
+            for key, value in values.items():
+                f.write(f"{key},{value}\n")
     else:
-        raise ValueError('the delimiter {} is not supported'.\
-                                       format(delimiter))
+        raise ValueError(f"the delimiter {delimiter} is not supported")
