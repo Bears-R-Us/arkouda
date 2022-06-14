a7e45b149113eb0e7ecb9b4d960207860e66a7cb
joshmarshall1 <65456732+joshmarshall1@users.noreply.github.com>
2022-06-14 14:36:35 -0400

    Closes #1386 - Generic `unregister_by_name()` method (#1510)
    * Adding generic unregister by name method and test case
    
    * Adding doc string
    
    Co-authored-by: joshmarshall1 <josh@MSI.localdomain>

--- tests/util_test.py
+++ tests/util_test.py
@@ -1,7 +1,9 @@
+import numpy as np
 from base_test import ArkoudaTest
 from context import arkouda as ak
 
-from arkouda.util import attach
+from arkouda import list_registry
+from arkouda.util import attach, unregister_by_name
 
 
 class utilTest(ArkoudaTest):
@@ -79,3 +81,60 @@ class utilTest(ArkoudaTest):
         self.assertListEqual(s2_attach.values.to_ndarray().tolist(), s2.values.to_ndarray().tolist())
         s2Eq = s2_attach.index == s2.index
         self.assertTrue(all(s2Eq.to_ndarray()))
+
+    def test_unregister_by_name(self):
+        # Register the four supported object types
+        # pdarray
+        pda = ak.arange(10)
+        pda.register("pdaUnregisterTest")
+        self.assertTrue(pda.is_registered())
+
+        # Strings
+        s1 = ak.array(["123", "abc", "def"])
+        s1.register("stringsUnregisterTest")
+        self.assertTrue(s1.is_registered())
+
+        # Categorical
+        s2 = ak.array(["abc", "123", "abc"])
+        cat = ak.Categorical(s2)
+        cat.register("catUnregisterTest")
+        self.assertTrue(cat.is_registered())
+
+        # Series
+        # Single Index
+        s3 = ak.Series(
+            index=ak.array(np.random.randint(0, 20, 10)), data=ak.array(np.random.randint(0, 20, 10))
+        )
+        s3.register("seriesSingleTest")
+        self.assertTrue(s3.is_registered())
+
+        multiInd = []
+        for x in range(3):
+            multiInd.append(ak.array(np.random.randint(0, 20, 10)))
+        s4 = ak.Series(index=multiInd, data=ak.array(np.random.randint(0, 20, 10)))
+        s4.register("seriesMultiTest")
+        registry = list_registry()
+        # Series.is_registered() does not support multiIndex
+        self.assertTrue("seriesMultiTest_value" in registry)
+        self.assertTrue("seriesMultiTest_key_0" in registry)
+        self.assertTrue("seriesMultiTest_key_1" in registry)
+        self.assertTrue("seriesMultiTest_key_2" in registry)
+
+        unregister_by_name("pdaUnregisterTest")
+        self.assertFalse(pda.is_registered())
+
+        unregister_by_name("stringsUnregisterTest")
+        self.assertFalse(s1.is_registered())
+
+        unregister_by_name("catUnregisterTest")
+        self.assertFalse(cat.is_registered())
+
+        unregister_by_name("seriesSingleTest")
+        self.assertFalse(s3.is_registered())
+
+        unregister_by_name("seriesMultiTest")
+        registry = list_registry()
+        self.assertFalse("seriesMultiTest_value" in registry)
+        self.assertFalse("seriesMultiTest_key_0" in registry)
+        self.assertFalse("seriesMultiTest_key_1" in registry)
+        self.assertFalse("seriesMultiTest_key_2" in registry)
