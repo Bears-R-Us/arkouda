3e8b0ab68876863e3d47ffc575b0ba7565bde51e
mhmerrill <48804335+mhmerrill@users.noreply.github.com>
2022-06-14 18:38:57 +0000

    Deploying to gh-pages from @ Bears-R-Us/arkouda@a7e45b149113eb0e7ecb9b4d960207860e66a7cb 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.gf9a7fa2',
+    VERSION: '0+untagged.1.ga7e45b1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
