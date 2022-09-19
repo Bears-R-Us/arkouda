cecab774a909c17bc3984defc9129c9813e05079
reuster986 <reuster986@gmail.com>
2022-09-19 08:56:35 -0400

    yet another test vector

--- tests/setops_test.py
+++ tests/setops_test.py
@@ -292,7 +292,7 @@ class SetOpsTest(ArkoudaTest):
         b2 = ak.array(d)
         t = ak.union1d([a1, a2], [b1, b2])
         self.assertListEqual(["xyz", "def", "abc"], t[0].to_list())
-        self.assertListEqual(["456", "0", "123"], t[1].to_list())
+        self.assertListEqual(["0", "456", "123"], t[1].to_list())
 
         # Test for Categorical
         cat_a1 = ak.Categorical(a1)
@@ -301,7 +301,7 @@ class SetOpsTest(ArkoudaTest):
         cat_b2 = ak.Categorical(b2)
         t = ak.union1d([cat_a1, cat_a2], [cat_b1, cat_b2])
         self.assertListEqual(["xyz", "def", "abc"], t[0].to_list())
-        self.assertListEqual(["123", "0", "456"], t[1].to_list())
+        self.assertListEqual(["0", "456", "123"], t[1].to_list())
 
     def testIn1d(self):
         pdaOne = ak.array([-1, 0, 1, 3])
