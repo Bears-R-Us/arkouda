02668199a6f80b3d7e0a97c32734a326e3cbc9e4
reuster986 <reuster986@gmail.com>
2022-09-19 09:10:36 -0400

    test vector

--- tests/setops_test.py
+++ tests/setops_test.py
@@ -300,7 +300,7 @@ class SetOpsTest(ArkoudaTest):
         cat_b1 = ak.Categorical(b1)
         cat_b2 = ak.Categorical(b2)
         t = ak.union1d([cat_a1, cat_a2], [cat_b1, cat_b2])
-        self.assertListEqual(["xyz", "def", "abc"], t[0].to_list())
+        self.assertListEqual(["abc", "xyz", "def"], t[0].to_list())
         self.assertListEqual(["0", "456", "123"], t[1].to_list())
 
     def testIn1d(self):
