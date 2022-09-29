3582de63786d7edc96599c1cf7f5b855ca2ac2ad
reuster986 <reuster986@gmail.com>
2022-09-29 15:45:43 +0000

    Deploying to gh-pages from @ Bears-R-Us/arkouda@b86adeb843275b0b86553534ede632acef4d15e2 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.g35c0395',
+    VERSION: '0+untagged.1.gb86adeb',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
