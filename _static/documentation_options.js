71fabd81c7ec888c90fd4cf5dd59d6c61906d2f4
joshmarshall1 <65456732+joshmarshall1@users.noreply.github.com>
2022-11-01 17:16:32 +0000

    Deploying to gh-pages from @ Bears-R-Us/arkouda@f1809651029d304fee70e1717b1dc0ac28564012 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.gfc18771',
+    VERSION: '0+untagged.1.gf180965',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
