711d5c9dbe6f2e28e4317671c0aa75607beffc92
Ethan-DeBandi99 <16845933+Ethan-DeBandi99@users.noreply.github.com>
2022-11-21 18:33:43 +0000

    Deploying to gh-pages from @ Bears-R-Us/arkouda@d2985dac7dcb5e286673df6d928c6c69768068bd 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.ge84196d',
+    VERSION: '0+untagged.1.gd2985da',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
