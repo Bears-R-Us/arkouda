a87febbf0ec4cecdb82e4e67fba9c9ce12c32b20
pierce314159 <48131946+pierce314159@users.noreply.github.com>
2022-12-02 18:22:13 +0000

    Deploying to gh-pages from @ Bears-R-Us/arkouda@9d2889cec5268fb27fe67494a94b1444474e6f08 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.g9cf2f29',
+    VERSION: '0+untagged.1.g9d2889c',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
