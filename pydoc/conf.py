09eab55b20d7a0e3af4870ce589d075d23e0c3ab
Ethan-DeBandi99 <16845933+Ethan-DeBandi99@users.noreply.github.com>
2022-12-20 11:28:16 -0500

    Closes #1979 - Update Sphinx Theme (#1980)
    * Updates to use sphinx theme. Need to add the pip install required.
    
    * Updating requirements to include sphinx theme

--- pydoc/conf.py
+++ pydoc/conf.py
@@ -58,7 +58,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+import sphinx_theme
+html_theme = 'stanford_theme'
+html_theme_path = [sphinx_theme.get_html_theme_path('stanford-theme')]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
