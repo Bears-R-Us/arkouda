f669dd50ae722c1168887f747b5e2615166303db
Ethan-DeBandi99 <16845933+Ethan-DeBandi99@users.noreply.github.com>
2022-12-21 14:50:20 -0500

    Removing unneeded comments.

--- pydoc/conf.py
+++ pydoc/conf.py
@@ -62,9 +62,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# import sphinx_theme
+
 html_theme = 'furo'
-# html_theme_path = [sphinx_theme.get_html_theme_path('stanford-theme')]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
