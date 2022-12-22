d66d0154faf0cddea08295042639df62851926f7
jaketrookman <jaketrookman@users.noreply.github.com>
2022-12-22 10:51:19 -0500

    testing update

--- tests/operator_tests.py
+++ tests/operator_tests.py
@@ -305,9 +305,8 @@ class OperatorsTest(ArkoudaTest):
         # Adding support to binopvv to correctly handle uint and bool types
         ak_uint = ak.arange(10, dtype=ak.uint64)
         ak_bool = ak_uint % 2 == 0
-        self.assertTrue(np.allclose((ak_uint + ak_bool).to_ndarray(),
-                                    (ak.arange(10) + ak_bool).to_ndarray(), equal_nan=True))
-
+        self.assertListEqual((ak_uint + ak_bool).tolist(), (ak.arange(10) + ak_bool).to_list())
+        
     def test_float_uint_binops(self):
         # Test fix for issue #1620
         ak_uint = ak.array([5], dtype=ak.uint64)
