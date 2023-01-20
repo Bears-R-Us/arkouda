91ebddb53e6efbf920699a645a321eec8b8d7160
jaketrookman <jaketrookman@users.noreply.github.com>
2023-01-20 14:56:58 -0500

    cleaning up import statements, only using 1 selected function on benchmark

--- benchmarks/run_benchmarks.py
+++ benchmarks/run_benchmarks.py
@@ -16,7 +16,7 @@ benchmark_dir = os.path.dirname(__file__)
 util_dir = os.path.join(benchmark_dir, "..", "server_util", "test")
 sys.path.insert(0, os.path.abspath(util_dir))
 
-from server_util.test.server_test_util import get_arkouda_numlocales, start_arkouda_server
+from server_util.test.server_test_util import get_arkouda_numlocales, start_arkouda_server, run_client
 
 logging.basicConfig(level=logging.INFO)
 
