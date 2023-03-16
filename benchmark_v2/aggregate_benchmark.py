f6886956459a82dc93071189e297668b24116dff
joshmarshall1 <65456732+joshmarshall1@users.noreply.github.com>
2023-03-16 15:07:32 -0400

    Closes #2219 - Implementation of v2 aggregation benchmark (#2221)
    * Initial implementation of v2 aggregation benchmark
    
    * Updatting config to proper structure
    
    * Updating extra_info to use consistent keys
    
    ---------
    
    Co-authored-by: joshmarshall1 <josh@MSI.localdomain>

---
+++ benchmark_v2/aggregate_benchmark.py
@@ -0,0 +1,43 @@
+import arkouda as ak
+import pytest
+
+
+def setup_agg(t="int"):
+    cfg = ak.get_config()
+    N = pytest.prob_size * cfg["numLocales"]
+
+    # Sort keys so that aggregations will not have to permute values
+    # We just want to measure aggregation time, not gather
+    keys = ak.sort(ak.randint(0, 2**32, N, seed=pytest.seed))
+    intvals = ak.randint(0, 2**16, N, seed=(pytest.seed + 1 if pytest.seed is not None else None))
+    g = ak.GroupBy(keys, assume_sorted=True)
+
+    if t == "int":
+        return g, intvals
+    else:
+        boolvals = (intvals % 2) == 0
+        return g, boolvals
+
+
+def run_agg(g, vals, op):
+    g.aggregate(vals, op)
+
+    return vals.size + vals.itemsize
+
+
+@pytest.mark.benchmark(group="GroupBy.aggregate")
+@pytest.mark.parametrize("op", ak.GroupBy.Reductions)
+def bench_aggs(benchmark, op):
+    if op in ["any", "all"]:
+        g, vals = setup_agg("bool")
+    else:
+        g, vals = setup_agg()
+
+    numBytes = benchmark.pedantic(run_agg, args=(g, vals, op), rounds=pytest.trials)
+    benchmark.extra_info[
+        "description"
+    ] = f"Measures performance of GroupBy.aggregate using the {op} operator."
+    benchmark.extra_info["problem_size"] = pytest.prob_size
+    benchmark.extra_info["transfer_rate"] = "{:.4f} GiB/sec".format(
+        (numBytes / benchmark.stats["mean"]) / 2**30
+    )
