83cb8d65018a1a88a51ff5bcc421e94aab050d37
Nate Shadeck <TheOddCzar789@users.noreply.github.com>
2023-07-14 23:21:03 -0400

    Paramaterize size variable

--- PROTO_tests/tests/bigint_agg_test.py
+++ PROTO_tests/tests/bigint_agg_test.py
@@ -3,30 +3,31 @@ import numpy as np
 
 import pytest
 
-SIZE = 5
-
 
 class TestBigInt:
-    def test_negative(self):
+    @pytest.mark.parametrize("size", pytest.prob_size)
+    def test_negative(self, size):
         # test with negative bigint values
-        arr = -1 * ak.randint(0, 2**32, SIZE)
+        arr = -1 * ak.randint(0, 2**32, size)
         bi_neg = ak.cast(arr, ak.bigint)
         res = gather_scatter(bi_neg)
         assert bi_neg.to_list() == res.to_list()
 
-    def test_large(self):
+    @pytest.mark.parametrize("size", pytest.prob_size)
+    def test_large(self, size):
         # test with 256 bit bigint values
-        top_bits = ak.randint(0, 2**32, SIZE, dtype=ak.uint64)
-        mid_bits1 = ak.randint(0, 2**32, SIZE, dtype=ak.uint64)
-        mid_bits2 = ak.randint(0, 2**32, SIZE, dtype=ak.uint64)
-        bot_bits = ak.randint(0, 2**32, SIZE, dtype=ak.uint64)
+        top_bits = ak.randint(0, 2**32, size, dtype=ak.uint64)
+        mid_bits1 = ak.randint(0, 2**32, size, dtype=ak.uint64)
+        mid_bits2 = ak.randint(0, 2**32, size, dtype=ak.uint64)
+        bot_bits = ak.randint(0, 2**32, size, dtype=ak.uint64)
         bi_arr = ak.bigint_from_uint_arrays([top_bits, mid_bits1, mid_bits2, bot_bits])
         res = gather_scatter(bi_arr)
         assert bi_arr.to_list() == res.to_list()
 
-    def test_zero(self):
+    @pytest.mark.parametrize("size", pytest.prob_size)
+    def test_zero(self, size):
         # test all zero bigint assignments
-        all_zero = ak.zeros(SIZE, dtype=ak.bigint)
+        all_zero = ak.zeros(size, dtype=ak.bigint)
         res = gather_scatter(all_zero)
         assert all_zero.to_list() == res.to_list()
 
@@ -41,9 +42,10 @@ class TestBigInt:
         res = gather_scatter(bi_arr)
         assert bi_arr.to_list() == res.to_list()
 
-    def test_change_size(self):
+    @pytest.mark.parametrize("size", pytest.prob_size)
+    def test_change_size(self, size):
         # Go from 256 bigint values down to just 1
-        bits = ak.randint(0, 2**32, SIZE, dtype=ak.uint64)
+        bits = ak.randint(0, 2**32, size, dtype=ak.uint64)
         bi_arr = ak.bigint_from_uint_arrays([bits, bits, bits, bits])
         res = ak.ones_like(bi_arr)
         bi_arr[:] = res
