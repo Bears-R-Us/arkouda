3ab2205329fbb44cf9b3677b678c140a036d2703
Ben McDonald <46734217+bmcdonald3@users.noreply.github.com>
2023-07-28 10:25:34 -0700

    Move receive_dataframe to IO

--- arkouda/dataframe.py
+++ arkouda/dataframe.py
@@ -44,7 +44,6 @@ __all__ = [
     "intersect",
     "invert_permutation",
     "intx",
-    "receive_dataframe",
 ]
 
 
@@ -2686,43 +2685,3 @@ def invert_permutation(perm):
     if (unique(perm).size != perm.size) and (perm.size != rng + 1):
         raise ValueError("The array is not a permutation.")
     return coargsort([perm, arange(perm.size)])
-
-
-def receive_dataframe(hostname : str, port):
-    """
-    Receive a pdarray sent by `dataframe.transfer()`.
-
-    Parameters
-    ----------
-    hostname : str
-        The hostname of the dataframe that sent the array
-    port : int_scalars
-        The port to send the dataframe over. This needs to be an
-        open port (i.e., not one that the Arkouda server is
-        running on). This will open up `numLocales` ports,
-        each of which in succession, so will use ports of the
-        range {port..(port+numLocales)} (e.g., running an
-        Arkouda server of 4 nodes, port 1234 is passed as
-        `port`, Arkouda will use ports 1234, 1235, 1236,
-        and 1237 to send the array data).
-        This port much match the port passed to the call to
-        `pdarray.send_array()`.
-
-    Returns
-    -------
-    pdarray
-        The dataframe sent from the sending server to the
-        current receiving server.
-
-    Raises
-    ------
-    ValueError
-        Raised if the op is not within the pdarray.BinOps set
-    TypeError
-        Raised if other is not a pdarray or the pdarray.dtype is not
-        a supported dtype
-    """
-    rep_msg = generic_msg(cmd="receiveDataframe", args={"hostname": hostname,
-                                                        "port"    : port})
-    rep = json.loads(rep_msg)
-    return DataFrame(_build_objects(rep))
--- arkouda/io.py
+++ arkouda/io.py
@@ -43,6 +43,7 @@ __all__ = [
     "snapshot",
     "restore",
     "receive",
+    "receive_dataframe",
 ]
 
 ARKOUDA_HDF5_FILE_METADATA_GROUP = "_arkouda_metadata"
@@ -2046,3 +2047,43 @@ def receive(hostname : str, port):
                                                     "port"    : port})
     rep = json.loads(rep_msg)
     return _build_objects(rep)
+
+
+def receive_dataframe(hostname : str, port):
+    """
+    Receive a pdarray sent by `dataframe.transfer()`.
+
+    Parameters
+    ----------
+    hostname : str
+        The hostname of the dataframe that sent the array
+    port : int_scalars
+        The port to send the dataframe over. This needs to be an
+        open port (i.e., not one that the Arkouda server is
+        running on). This will open up `numLocales` ports,
+        each of which in succession, so will use ports of the
+        range {port..(port+numLocales)} (e.g., running an
+        Arkouda server of 4 nodes, port 1234 is passed as
+        `port`, Arkouda will use ports 1234, 1235, 1236,
+        and 1237 to send the array data).
+        This port much match the port passed to the call to
+        `pdarray.send_array()`.
+
+    Returns
+    -------
+    pdarray
+        The dataframe sent from the sending server to the
+        current receiving server.
+
+    Raises
+    ------
+    ValueError
+        Raised if the op is not within the pdarray.BinOps set
+    TypeError
+        Raised if other is not a pdarray or the pdarray.dtype is not
+        a supported dtype
+    """
+    rep_msg = generic_msg(cmd="receiveDataframe", args={"hostname": hostname,
+                                                        "port"    : port})
+    rep = json.loads(rep_msg)
+    return DataFrame(_build_objects(rep))
