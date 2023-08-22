2aaaac4d10f91932adf1ee636e29bff69508102c
pierce <48131946+pierce314159@users.noreply.github.com>
2023-08-21 20:12:11 -0400

    Closes #2694: `extrema_test.py` conversion for new test framework (#2696)
    This PR (closes #2694) converts `extrema_test.py` to new test framework
    
    Co-authored-by: Pierce Hayes <pierce314159@users.noreply.github.com>

---
+++ PROTO_tests/tests/extrema_test.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pytest
+
+import arkouda as ak
+
+NO_UINT = ["int64", "float64", "bool"]
+
+
+def make_np_arrays(size, dtype):
+    if dtype == "int64":
+        return np.random.randint(-(2**32), 2**32, size=size, dtype=dtype)
+    elif dtype == "float64":
+        return np.random.uniform(-(2**32), 2**32, size=size)
+    elif dtype == "bool":
+        return np.random.randint(0, 1, size=size, dtype=dtype)
+    return None
+
+
+class TestExtrema:
+    @pytest.mark.parametrize("prob_size", pytest.prob_size)
+    @pytest.mark.parametrize("dtype", ["int64", "float64"])
+    def test_extrema(self, prob_size, dtype):
+        # TODO add testing for uint once #2695 is completed
+        pda = ak.randint(-(2**32), 2**32, size=prob_size, dtype=dtype)
+        ak_sorted = ak.sort(pda)
+        K = prob_size // 2
+
+        # compare minimums against first K elements from sorted array
+        assert (ak.mink(pda, K) == ak_sorted[:K]).all()
+        assert (pda[ak.argmink(pda, K)] == ak_sorted[:K]).all()
+
+        # compare maximums against last K elements from sorted array
+        assert (ak.maxk(pda, K) == ak_sorted[-K:]).all()
+        assert (pda[ak.argmaxk(pda, K)] == ak_sorted[-K:]).all()
+
+    @pytest.mark.parametrize("dtype", NO_UINT)
+    def test_argmin_and_argmax(self, dtype):
+        # TODO add testing for uint once #2695 is completed
+        np_arr = make_np_arrays(1000, dtype)
+        ak_arr = ak.array(np_arr)
+
+        assert np_arr.argmin() == ak_arr.argmin()
+        assert np_arr.argmax() == ak_arr.argmax()
+
+    def test_error_handling(self):
+        test_array = ak.randint(0, 100, 100)
+        for op in ak.mink, ak.maxk, ak.argmink, ak.argmaxk:
+            with pytest.raises(TypeError):
+                op(list(range(10)), 1)
+
+            with pytest.raises(TypeError):
+                op(test_array, "1")
+
+            with pytest.raises(ValueError):
+                op(test_array, -1)
+
+            with pytest.raises(ValueError):
+                op(ak.array([]), 1)
