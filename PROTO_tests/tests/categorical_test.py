994327d20ef54e3fa5c4bd17de6382fee85b42d5
Amanda Potts <ajpotts@users.noreply.github.com>
2024-05-31 16:21:51 -0400

    Closes #Ticket 3275 deprecate tests/categorical_test

--- PROTO_tests/tests/categorical_test.py
+++ PROTO_tests/tests/categorical_test.py
@@ -1,8 +1,27 @@
+import os
+import tempfile
+
 import numpy as np
 import pytest
 
 import arkouda as ak
+from arkouda import io, io_util
 from arkouda.categorical import Categorical
+
+
+@pytest.fixture
+def df_test_base_tmp(request):
+    df_test_base_tmp = "{}/.categorical_test".format(os.getcwd())
+    io_util.get_directory(df_test_base_tmp)
+
+    # Define a finalizer function for teardown
+    def finalizer():
+        # Clean up any resources if needed
+        io_util.delete_directory(df_test_base_tmp)
+
+    # Register the finalizer to ensure cleanup
+    request.addfinalizer(finalizer)
+    return df_test_base_tmp
 
 
 class TestCategorical:
@@ -24,6 +43,24 @@ class TestCategorical:
 
     def create_basic_categorical(self, prefix: str = "string", size: int = 10) -> ak.Categorical:
         return ak.Categorical(ak.array([f"{prefix} {i}" for i in range(size)]))
+
+    def create_randomized_categorical(self) -> ak.Categorical:
+        return ak.Categorical(
+            ak.array(
+                [
+                    "string",
+                    "string1",
+                    "non-string",
+                    "non-string2",
+                    "string",
+                    "non-string",
+                    "string3",
+                    "non-string2",
+                    "string",
+                    "non-string",
+                ]
+            )
+        )
 
     def test_basic_categorical(self):
         prefix, size = "string", 10
@@ -72,6 +109,9 @@ class TestCategorical:
         assert cat.endswith("1").any()
         assert cat.startswith("string").all()
 
+        assert cat.contains("\\w", regex=True).all()
+        assert cat.endswith("ing \\d", regex=True).all()
+
     def test_group(self):
         non_unique_cat = self.non_unique_cat
         grouped = non_unique_cat[non_unique_cat.group()]
@@ -91,6 +131,24 @@ class TestCategorical:
             ak.array(["string", "string1", "string3", "non-string", "non-string2"])
         )
         assert non_unique_cat.unique().to_list() == unique_cat.to_list()
+
+    def test_to_ndarray(self):
+        cat = self.create_randomized_categorical()
+        ndcat = np.array(
+            [
+                "string",
+                "string1",
+                "non-string",
+                "non-string2",
+                "string",
+                "non-string",
+                "string3",
+                "non-string2",
+                "string",
+                "non-string",
+            ]
+        )
+        assert (cat.to_ndarray() == ndcat).all()
 
     def test_to_list(self):
         assert self.non_unique_cat.to_list() == np.array(self.non_unique_strs).tolist()
@@ -220,6 +278,102 @@ class TestCategorical:
         b = ak.Categorical(ak.array(["b"]))
         assert ak.concatenate((a, b), ordered=False).to_list() == ak.array(["a", "b"]).to_list()
 
+    def test_save_and_load_categorical(self, df_test_base_tmp):
+        """
+        Test to save categorical to hdf5 and read it back successfully
+        """
+        num_elems = 51  # create_basic_categorical starts counting at 1, so the size is really off by one
+        cat = self.create_basic_categorical(size=num_elems)
+        with pytest.raises(ValueError):
+            # Expect error for mode not being append or truncate
+            cat.to_hdf("foo", dataset="bar", mode="not_allowed")
+
+        with tempfile.TemporaryDirectory(dir=df_test_base_tmp) as tmp_dirname:
+            dset_name = "categorical_array"  # name of categorical array
+
+            # Test the save functionality & confirm via h5py
+            cat.to_hdf(f"{tmp_dirname}/cat-save-test", dataset=dset_name)
+
+            import h5py
+
+            f = h5py.File(tmp_dirname + "/cat-save-test_LOCALE0000", mode="r")
+            keys = list(f.keys())
+            if io.ARKOUDA_HDF5_FILE_METADATA_GROUP in keys:  # Ignore the metadata group if it exists
+                keys.remove(io.ARKOUDA_HDF5_FILE_METADATA_GROUP)
+            assert len(keys) == 1, f"Expected 1 key, {dset_name}"
+            assert [dset_name] == keys
+            d = f[dset_name]
+            f.close()
+
+            # Now try to read them back with load_all
+            x = ak.load_all(path_prefix=f"{tmp_dirname}/cat-save-test")
+            assert dset_name in x
+            cat_from_hdf = x[dset_name]
+
+            expected_categories = [f"string {i}" for i in range(0, num_elems)] + ["N/A"]
+
+            # Note assertCountEqual asserts a and b have the same elements
+            # in the same amount regardless of order
+            assert cat_from_hdf.categories.to_list() == expected_categories
+
+            # Asserting the optional components and sizes are correct
+            # for both constructors should be sufficient
+            assert cat_from_hdf.segments is not None
+            assert cat_from_hdf.permutation is not None
+            print(f"==> cat_from_hdf.size:{cat_from_hdf.size}")
+            assert cat_from_hdf.size == num_elems
+
+    def test_save_and_load_categorical_multi(self, df_test_base_tmp):
+        """
+        Test to build a pseudo dataframe with multiple
+        categoricals, pdarrays, strings objects and successfully
+        write/read it from HDF5
+        """
+        c1 = self.create_basic_categorical(prefix="c1", size=51)
+        c2 = self.create_basic_categorical(prefix="c2", size=52)
+        pda1 = ak.zeros(51)
+        strings1 = ak.random_strings_uniform(9, 10, 52)
+
+        with tempfile.TemporaryDirectory(dir=df_test_base_tmp) as tmp_dirname:
+            df = {"cat1": c1, "cat2": c2, "pda1": pda1, "strings1": strings1}
+            ak.to_hdf(df, f"{tmp_dirname}/cat-save-test")
+            x = ak.load_all(path_prefix=f"{tmp_dirname}/cat-save-test")
+            assert len(x.items()) == 4
+            # Note assertCountEqual asserts a and b have the same
+            # elements in the same amount regardless of order
+            assert x["cat1"].categories.to_list() == c1.categories.to_list()
+            assert x["cat2"].categories.to_list() == c2.categories.to_list()
+            assert x["pda1"].to_list() == pda1.to_list()
+            assert x["strings1"].to_list() == strings1.to_list()
+
+    def test_hdf_update(self, df_test_base_tmp):
+        num_elems = 51  # create_basic_categorical starts counting at 1, so the size is really off by one
+        cat = self.create_basic_categorical(size=num_elems)
+        with tempfile.TemporaryDirectory(dir=df_test_base_tmp) as tmp_dirname:
+            dset_name = "categorical_array"  # name of categorical array
+            cat.to_hdf(f"{tmp_dirname}/cat-save-test", dataset=dset_name)
+
+            dset_name2 = "to_replace"
+            cat.to_hdf(f"{tmp_dirname}/cat-save-test", dataset=dset_name2, mode="append")
+
+            dset_name3 = "cat_array2"
+            cat.to_hdf(f"{tmp_dirname}/cat-save-test", dataset=dset_name3, mode="append")
+
+            replace_cat = self.create_basic_categorical(size=23)
+            replace_cat.update_hdf(f"{tmp_dirname}/cat-save-test", dataset=dset_name2)
+
+            data = ak.read_hdf(f"{tmp_dirname}/cat-save-test_*")
+            assert dset_name in data
+            assert dset_name2 in data
+            assert dset_name3 in data
+
+            d = data[dset_name2]
+            assert d.codes.to_list() == replace_cat.codes.to_list()
+            assert d.permutation.to_list() == replace_cat.permutation.to_list()
+            assert d.segments.to_list() == replace_cat.segments.to_list()
+            assert d._akNAcode.to_list() == replace_cat._akNAcode.to_list()
+            assert d.categories.to_list() == replace_cat.categories.to_list()
+
     def test_unused_categories_logic(self):
         # Reproducer for issue #990
         s = ak.array([str(i) for i in range(10)])
@@ -265,6 +419,15 @@ class TestCategorical:
         args = ak.array([3, 2, 1, 0])
         ret = ak.lookup(keys, values, args)
         assert ret.to_list() == ["C", "B", "A", "N/A"]
+
+    def test_deletion(self):
+        cat = ak.Categorical(ak.array(["a", "b", "c"]))
+        # validate registration with server
+        assert len(ak.list_symbol_table()) > 0
+
+        # set to none and validate no entries in symbol table
+        cat = None
+        assert len(ak.list_symbol_table()), 0
 
     def test_sort(self):
         rand_cats = ak.random_strings_uniform(1, 16, 10)
