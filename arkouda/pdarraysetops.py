5d7e4d572c49bcff86407dd4f86f36d76bacb5d2
Tess Hayes <stress-tess@users.noreply.github.com>
2024-06-11 16:20:25 -0400

    Part of #3229: Re-enable find implementation of `indexof1d`
    This PR is part of #3229. Re-enable the `find` implementation. I modified it to have a more optimized way of finding all occurences (which will hopefully also solve the issue we were seeing in the CI). While I'm not sure if this is the best way of calculating this, it's no longer the bottleneck and it gets better performance than the current implementation.
    
    Previously I added seeds to the `indexof1d` test, so it will be easier to reproduce any failures. I'm not closing the issue yet since there's still some code cleanup to be done, and I want to leave exisiting code to make it easier to fall back to a similar approach in the event that the `find` way still has issues.

--- arkouda/pdarraysetops.py
+++ arkouda/pdarraysetops.py
@@ -275,7 +275,6 @@ def indexof1d(query: groupable, space: groupable) -> pdarray:
     RuntimeError
         Raised if the dtype of either array is not supported
     """
-    # from arkouda.alignment import find as akfind
     from arkouda.categorical import Categorical as Categorical_
 
     if isinstance(query, (pdarray, Strings, Categorical_)):
@@ -284,15 +283,16 @@ def indexof1d(query: groupable, space: groupable) -> pdarray:
         elif isinstance(query, pdarray) and not isinstance(space, pdarray):
             raise TypeError("If keys is pdarray, arr must also be pdarray")
 
-    repMsg = generic_msg(
-        cmd="indexof1d",
-        args={"keys": query, "arr": space},
-    )
-    return create_pdarray(cast(str, repMsg))
+    # repMsg = generic_msg(
+    #     cmd="indexof1d",
+    #     args={"keys": query, "arr": space},
+    # )
+    # return create_pdarray(cast(str, repMsg))
 
-    # TODO see issue #3229 reverted back to old implementation until we can investigate
-    # found = akfind(query, space, all_occurrences=True, remove_missing=True)
-    # return found if isinstance(found, pdarray) else found.values
+    from arkouda.alignment import find as akfind
+
+    found = akfind(query, space, all_occurrences=True, remove_missing=True)
+    return found if isinstance(found, pdarray) else found.values
 
 
 # fmt: off
