105f166cee71f75425158a370008161fb7363e79
tess <48131946+stress-tess@users.noreply.github.com>
2024-06-19 19:37:08 -0400

    Related to #3346: Disable `3.12` / `3.x` workflows (#3349)
    * pin python<3.12.4
    
    * disable 3.12 and 3.x for the time being
    
    ---------
    
    Co-authored-by: Tess Hayes <stress-tess@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -108,7 +108,7 @@ setup(
     # and refuse to install the project if the version does not match. If you
     # do not support Python 2, you can simplify this to '>=3.5' or similar, see
     # https://packaging.python.org/guides/distributing-packages-using-setuptools/#python-requires
-    python_requires=">=3.8",
+    python_requires=">=3.8,<3.12.4",
     # This field lists other packages that your project depends on to run.
     # Any package you put here will be installed by pip when your project is
     # installed, so they must be valid existing projects.
