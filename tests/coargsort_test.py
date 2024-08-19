38e6a2da3f7d8a726cfbb267e5554af8c6b9295e
Amanda Potts <ajpotts@users.noreply.github.com>
2024-08-19 13:57:23 -0400

    comment out tests for debugging purposes

--- tests/coargsort_test.py
+++ tests/coargsort_test.py
@@ -23,37 +23,34 @@ def make_ak_arrays(size, dtype, minimum=-(2**32), maximum=2**32):
 
 
 class TestCoargsort:
-    def test_temp(self):
-        print("TEST")
-        pass
-    # @pytest.mark.parametrize("prob_size", pytest.prob_size)
-    # @pytest.mark.parametrize("dtype", NUMERIC_TYPES)
-    # @pytest.mark.parametrize("algo", SortingAlgorithm)
-    # def test_coargsort_single_type(self, prob_size, dtype, algo):
-    #     # add test for large number of arrays and all permutations of mixed sizes
-    #     arr_lists = [
-    #         [make_ak_arrays(prob_size, dtype, 0, 2**63) for _ in range(10)],
-    #         *permutations(make_ak_arrays(prob_size, dtype, 0, 2**exp) for exp in (2, 4, 8)),
-    #     ]
-    #
-    #     for exp in 8 * 2, 8 * 4, 8 * 8:
-    #         a, b, c, d = (make_ak_arrays(prob_size, dtype, 0, 2**exp) for _ in range(4))
-    #         z = ak.zeros(prob_size, dtype=dtype)
-    #         n = make_ak_arrays(prob_size, dtype, -(2 ** (exp - 1)), 2**exp)
-    #         arr_lists.extend(
-    #             [[a], [n], [a, b], [b, a], [a, b, c, d], [z, b, c, d], [z, z, c, d], [z, z, z, d]]
-    #         )
-    #
-    #     for arr_list in arr_lists:
-    #         if not isinstance(arr_list, list):
-    #             arr_list = list(arr_list)
-    #         perm = ak.coargsort(arr_list, algo)
-    #         # TODO remove once ak.is_sorted is implemented for bigint
-    #         if dtype == "bigint":
-    #             # shift it down for is_cosorted check
-    #             arr_list[0] = ak.cast(arr_list[0] - 2**200, ak.int64)
-    #         assert ak.is_cosorted([arr[perm] for arr in arr_list])
-    #
+    @pytest.mark.parametrize("prob_size", pytest.prob_size)
+    @pytest.mark.parametrize("dtype", NUMERIC_TYPES)
+    @pytest.mark.parametrize("algo", SortingAlgorithm)
+    def test_coargsort_single_type(self, prob_size, dtype, algo):
+        # add test for large number of arrays and all permutations of mixed sizes
+        arr_lists = [
+            [make_ak_arrays(prob_size, dtype, 0, 2**63) for _ in range(10)],
+            *permutations(make_ak_arrays(prob_size, dtype, 0, 2**exp) for exp in (2, 4, 8)),
+        ]
+
+        for exp in 8 * 2, 8 * 4, 8 * 8:
+            a, b, c, d = (make_ak_arrays(prob_size, dtype, 0, 2**exp) for _ in range(4))
+            z = ak.zeros(prob_size, dtype=dtype)
+            n = make_ak_arrays(prob_size, dtype, -(2 ** (exp - 1)), 2**exp)
+            arr_lists.extend(
+                [[a], [n], [a, b], [b, a], [a, b, c, d], [z, b, c, d], [z, z, c, d], [z, z, z, d]]
+            )
+
+        for arr_list in arr_lists:
+            if not isinstance(arr_list, list):
+                arr_list = list(arr_list)
+            perm = ak.coargsort(arr_list, algo)
+            # TODO remove once ak.is_sorted is implemented for bigint
+            if dtype == "bigint":
+                # shift it down for is_cosorted check
+                arr_list[0] = ak.cast(arr_list[0] - 2**200, ak.int64)
+            assert ak.is_cosorted([arr[perm] for arr in arr_list])
+
     # @pytest.mark.parametrize("prob_size", pytest.prob_size)
     # @pytest.mark.parametrize("algo", SortingAlgorithm)
     # def test_coargsort_mixed_types(self, prob_size, algo):
