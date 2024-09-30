b90155f8306c9167650bf72f81515dcea87e2b8c
ajpotts <amanda.j.potts@gmail.com>
2024-09-30 16:49:00 -0400

    Closes #3797 benchmark v2/str locality benchmark.py throws errors (#3800)
    * Closes #3793:  make benchmark errors
    
    * Closes #3797: benchmark_v2/str_locality_benchmark.py throws errors.
    
    ---------
    
    Co-authored-by: Amanda Potts <ajpotts@users.noreply.github.com>

--- benchmark_v2/str_locality_benchmark.py
+++ benchmark_v2/str_locality_benchmark.py
@@ -5,7 +5,7 @@ OPS = {
     "Hashing": lambda x: x.hash(),
     "Regex_Search": lambda x: x.contains(r"\d{3,5}\.\d{5,8}", regex=True),
     "Casting": lambda x: ak.cast(x, ak.float64),
-    "Scalar_Compare": lambda x: (x == "5.5")
+    "Scalar_Compare": lambda x: (x == "5.5"),
 }
 
 # Good - generates random Strings object with "good" locality
@@ -13,37 +13,28 @@ OPS = {
 LOCALITY = {"Good", "Poor"}
 
 
-RAND_DATA = None
-SORT_DATA = None
-
-
 def _generate_data(loc):
     """
     Generate the test data. In an interest to leverage the same data for the benchmark
     The data is all created at once.
     """
-    global RAND_DATA, SORT_DATA
-
-    # early out if already set
-    if loc == "Good" and RAND_DATA is not None:
-        return RAND_DATA
-    if loc == "Poor" and SORT_DATA is not None:
-        return SORT_DATA
 
     # otherwise set both and return the desired one.
     N = pytest.prob_size * ak.get_config()["numLocales"]
-    prefix = ak.random_strings_uniform(minlen=1, maxlen=16, size=N, seed=pytest.seed, characters="numeric")
+    prefix = ak.random_strings_uniform(
+        minlen=1, maxlen=16, size=N, seed=pytest.seed, characters="numeric"
+    )
     if pytest.seed is not None:
         pytest.seed += 1
-    suffix = ak.random_strings_uniform(minlen=1, maxlen=16, size=N, seed=pytest.seed, characters="numeric")
+    suffix = ak.random_strings_uniform(
+        minlen=1, maxlen=16, size=N, seed=pytest.seed, characters="numeric"
+    )
     random_strings = prefix.stick(suffix, delimiter=".")
-    RAND_DATA = random_strings
 
     perm = ak.argsort(random_strings.get_lengths())
     sorted_strings = random_strings[perm]
-    SORT_DATA = sorted_strings
 
-    return RAND_DATA if loc == "Good" else SORT_DATA
+    return random_strings if loc == "Good" else sorted_strings
 
 
 @pytest.mark.benchmark(group="String_Locality")
@@ -53,8 +44,11 @@ def bench_str_locality(benchmark, op, loc):
     data = _generate_data(loc)
     benchmark.pedantic(OPS[op], args=[data], rounds=pytest.trials)
 
-    benchmark.extra_info["description"] = "Measure the performance of various string operations on " \
-                                          "strings with good locality (random) and poor locality (sorted)."
+    benchmark.extra_info["description"] = (
+        "Measure the performance of various string operations on "
+        "strings with good locality (random) and poor locality (sorted)."
+    )
     benchmark.extra_info["problem_size"] = pytest.prob_size
     benchmark.extra_info["transfer_rate"] = "{:.4f} GiB/sec".format(
-        (data.nbytes / benchmark.stats["mean"]) / 2 ** 30)
+        (data.nbytes / benchmark.stats["mean"]) / 2**30
+    )
