28cb52c2879eaae3c4293726c2b14d7158cfd77f
Amanda Potts <ajpotts@users.noreply.github.com>
2025-03-21 09:58:22 -0400

    debug

--- arkouda/numpy/random/__init__.py
+++ arkouda/numpy/random/__init__.py
@@ -2,8 +2,8 @@ from ._generator import Generator, default_rng  # noqa
 from ._legacy import randint, standard_normal, uniform
 
 __all__ = [
-    "Generator",
-    "randint",
-    "standard_normal",
-    "uniform",
+    'Generator',
+    'randint',
+    'standard_normal',
+    'uniform',
 ]
