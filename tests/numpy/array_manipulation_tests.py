280837d12b27d6f68b83b85b5641213e09468e4d
1RyanK <146032320+1RyanK@users.noreply.github.com>
2025-04-23 19:51:46 -0400

    Closes #3329: hstack to match numpy (#4105)
    Since hstack uses concatenate in numpy, I redid concatenate on server
    side. It seems to work with normal pdarrays but not Strings or
    Categorical. I made some modifications on other things that also rely on
    ConcatenateMsg.chpl.
    
    I also updated vstack because I somewhat doubt that functioned properly
    before. I noticed that the code where the vstack and delete tests were
    (tests/array_manipulation_tests.py) was not included in the pytest.ini,
    so I added it. The tests for delete failed, but I didn't touch it, so I
    suspect that was ongoing prior to my adding it.
    
    While trying to clean things up a bit, I noticed some interesting
    behavior. Some things rely on the concatenate with ordered=False
    (meaning that when concatenating two arrays, the result may have the two
    arrays mixed together, rather than a whole block where you have the
    first, followed by everything in the second). Specifically, when
    comparing two Categorical objects together (i.e. for equality), it would
    concatenate the Strings of categories and also two aranges (which were,
    I guess, meant to associate the categories to numbers). Basically, it
    seemed to rely on the fact that they would get concatenated in the same
    way. This seems very sketchy to me. At some point we should probably
    further investigate set_categories in arkouda/categorical.py and the
    functions it calls. It takes a shockingly long time to test equality on
    two Categoricals with three things, but different categories. Definitely
    feels like optimizations are possible. Also ConcatenateMsg.chpl could
    probably benefit from optimization for Strings.
    
    Closes #3329: hstack to match numpy

--- tests/numpy/array_manipulation_tests.py
+++ tests/numpy/array_manipulation_tests.py
@@ -9,16 +9,187 @@ from arkouda.testing import assert_arkouda_array_equivalent
 class TestManipulationFunctions:
     @pytest.mark.skip_if_rank_not_compiled([2])
     def test_vstack(self):
-        # These tests will be implemented soon, hopefully.
-        """
         a = [ak.random.randint(0, 10, 25) for _ in range(4)]
         n = [x.to_ndarray() for x in a]
 
         n_vstack = np.vstack(n)
         a_vstack = ak.vstack(a)
 
-        assert n_vstack.tolist() == a_vstack.to_list()
-        """
+        assert_arkouda_array_equivalent(n_vstack, a_vstack)
+
+    @pytest.mark.skip_if_rank_not_compiled([2])
+    @pytest.mark.parametrize("size", pytest.prob_size)
+    @pytest.mark.parametrize("dtype", [int, ak.int64, ak.uint64, float, ak.float64, ak.bigint])
+    def test_vstack_with_dtypes(self, size, dtype):
+        if dtype == ak.bigint:
+            a = [ak.arange(2**200 + i * size, 2**200 + (i + 1) * size, dtype=dtype) for i in range(4)]
+            n = [np.arange(2**200 + i * size, 2**200 + (i + 1) * size) for i in range(4)]
+        else:
+            a = [ak.arange(i * size, (i + 1) * size, dtype=dtype) for i in range(4)]
+            n = [x.to_ndarray() for x in a]
+        n_vstack = np.vstack(n)
+        a_vstack = ak.vstack(a)
+
+        assert_arkouda_array_equivalent(n_vstack, a_vstack)
+
+    @pytest.mark.skip_if_rank_not_compiled([2])
+    @pytest.mark.parametrize("dtype", [int, ak.int64, ak.uint64, float, ak.float64, ak.bigint])
+    @pytest.mark.parametrize("shapes", [[(3,), (3,)], [(2, 3), (3,)], [(3, 3), (4, 3)]])
+    def test_vstack2D_with_shapes(self, dtype, shapes):
+        shape1, shape2 = shapes
+
+        shape1_prod = 1
+        for i in shape1:
+            shape1_prod = shape1_prod * i
+
+        shape2_prod = 1
+        for i in shape2:
+            shape2_prod = shape2_prod * i
+
+        if dtype == ak.bigint:
+            ak_a = ak.arange(2**200, 2**200 + shape1_prod, dtype=dtype).reshape(shape1)
+            ak_b = ak.arange(
+                2**200 + shape1_prod, 2**200 + (shape1_prod + shape2_prod), dtype=dtype
+            ).reshape(shape2)
+            ak_vstack = ak.vstack((ak_a, ak_b))
+
+            np_a = np.arange(2**200, 2**200 + shape1_prod).reshape(shape1)
+            np_b = np.arange(2**200 + shape1_prod, 2**200 + (shape1_prod + shape2_prod)).reshape(shape2)
+            np_vstack = np.vstack((np_a, np_b))
+        else:
+            ak_a = ak.arange(shape1_prod, dtype=dtype).reshape(shape1)
+            ak_b = ak.arange(shape1_prod, (shape1_prod + shape2_prod), dtype=dtype).reshape(shape2)
+            ak_vstack = ak.vstack((ak_a, ak_b))
+
+            np_a = np.arange(shape1_prod, dtype=dtype).reshape(shape1)
+            np_b = np.arange(shape1_prod, (shape1_prod + shape2_prod), dtype=dtype).reshape(shape2)
+            np_vstack = np.vstack((np_a, np_b))
+
+        assert_arkouda_array_equivalent(np_vstack, ak_vstack)
+
+    @pytest.mark.skip_if_rank_not_compiled([3])
+    @pytest.mark.parametrize("dtype", [int, ak.int64, ak.uint64, float, ak.float64, ak.bigint])
+    @pytest.mark.parametrize(
+        "shapes", [[(1, 2, 3), (1, 2, 3)], [(1, 2, 3), (2, 2, 3)], [(2, 2, 2), (4, 2, 2)]]
+    )
+    def test_vstack3D_with_shapes(self, dtype, shapes):
+        shape1, shape2 = shapes
+
+        shape1_prod = 1
+        for i in shape1:
+            shape1_prod = shape1_prod * i
+
+        shape2_prod = 1
+        for i in shape2:
+            shape2_prod = shape2_prod * i
+
+        if dtype == ak.bigint:
+            ak_a = ak.arange(2**200, 2**200 + shape1_prod, dtype=dtype).reshape(shape1)
+            ak_b = ak.arange(
+                2**200 + shape1_prod, 2**200 + (shape1_prod + shape2_prod), dtype=dtype
+            ).reshape(shape2)
+            ak_vstack = ak.vstack((ak_a, ak_b))
+
+            np_a = np.arange(2**200, 2**200 + shape1_prod).reshape(shape1)
+            np_b = np.arange(2**200 + shape1_prod, 2**200 + (shape1_prod + shape2_prod)).reshape(shape2)
+            np_vstack = np.vstack((np_a, np_b))
+        else:
+            ak_a = ak.arange(shape1_prod, dtype=dtype).reshape(shape1)
+            ak_b = ak.arange(shape1_prod, (shape1_prod + shape2_prod), dtype=dtype).reshape(shape2)
+            ak_vstack = ak.vstack((ak_a, ak_b))
+
+            np_a = np.arange(shape1_prod, dtype=dtype).reshape(shape1)
+            np_b = np.arange(shape1_prod, (shape1_prod + shape2_prod), dtype=dtype).reshape(shape2)
+            np_vstack = np.vstack((np_a, np_b))
+
+        assert_arkouda_array_equivalent(np_vstack, ak_vstack)
+
+    @pytest.mark.parametrize("size", pytest.prob_size)
+    @pytest.mark.parametrize("dtype", [int, ak.int64, ak.uint64, float, ak.float64, ak.bigint])
+    def test_hstack(self, size, dtype):
+        if dtype == ak.bigint:
+            a = [ak.arange(2**200 + i * size, 2**200 + (i + 1) * size, dtype=dtype) for i in range(4)]
+            n = [np.arange(2**200 + i * size, 2**200 + (i + 1) * size) for i in range(4)]
+        else:
+            a = [ak.arange(i * size, (i + 1) * size, dtype=dtype) for i in range(4)]
+            n = [x.to_ndarray() for x in a]
+
+        n_hstack = np.hstack(n)
+        a_hstack = ak.hstack(a)
+
+        assert_arkouda_array_equivalent(n_hstack, a_hstack)
+
+    @pytest.mark.skip_if_rank_not_compiled([2])
+    @pytest.mark.parametrize("dtype", [int, ak.int64, ak.uint64, float, ak.float64, ak.bigint])
+    @pytest.mark.parametrize("shapes", [[(3, 1), (3, 1)], [(2, 3), (2, 4)], [(3, 5), (3, 2)]])
+    def test_hstack2D_with_shapes(self, dtype, shapes):
+        shape1, shape2 = shapes
+
+        shape1_prod = 1
+        for i in shape1:
+            shape1_prod = shape1_prod * i
+
+        shape2_prod = 1
+        for i in shape2:
+            shape2_prod = shape2_prod * i
+
+        if dtype == ak.bigint:
+            ak_a = ak.arange(2**200, 2**200 + shape1_prod, dtype=dtype).reshape(shape1)
+            ak_b = ak.arange(
+                2**200 + shape1_prod, 2**200 + (shape1_prod + shape2_prod), dtype=dtype
+            ).reshape(shape2)
+            ak_hstack = ak.hstack((ak_a, ak_b))
+
+            np_a = np.arange(2**200, 2**200 + shape1_prod).reshape(shape1)
+            np_b = np.arange(2**200 + shape1_prod, 2**200 + (shape1_prod + shape2_prod)).reshape(shape2)
+            np_hstack = np.hstack((np_a, np_b))
+        else:
+            ak_a = ak.arange(shape1_prod, dtype=dtype).reshape(shape1)
+            ak_b = ak.arange(shape1_prod, (shape1_prod + shape2_prod), dtype=dtype).reshape(shape2)
+            ak_hstack = ak.hstack((ak_a, ak_b))
+
+            np_a = np.arange(shape1_prod, dtype=dtype).reshape(shape1)
+            np_b = np.arange(shape1_prod, (shape1_prod + shape2_prod), dtype=dtype).reshape(shape2)
+            np_hstack = np.hstack((np_a, np_b))
+
+        assert_arkouda_array_equivalent(np_hstack, ak_hstack)
+
+    @pytest.mark.skip_if_rank_not_compiled([3])
+    @pytest.mark.parametrize("dtype", [int, ak.int64, ak.uint64, float, ak.float64, ak.bigint])
+    @pytest.mark.parametrize(
+        "shapes", [[(1, 2, 3), (1, 2, 3)], [(1, 2, 3), (1, 1, 3)], [(2, 2, 2), (2, 4, 2)]]
+    )
+    def test_hstack3D_with_shapes(self, dtype, shapes):
+        shape1, shape2 = shapes
+
+        shape1_prod = 1
+        for i in shape1:
+            shape1_prod = shape1_prod * i
+
+        shape2_prod = 1
+        for i in shape2:
+            shape2_prod = shape2_prod * i
+
+        if dtype == ak.bigint:
+            ak_a = ak.arange(2**200, 2**200 + shape1_prod, dtype=dtype).reshape(shape1)
+            ak_b = ak.arange(
+                2**200 + shape1_prod, 2**200 + (shape1_prod + shape2_prod), dtype=dtype
+            ).reshape(shape2)
+            ak_hstack = ak.hstack((ak_a, ak_b))
+
+            np_a = np.arange(2**200, 2**200 + shape1_prod).reshape(shape1)
+            np_b = np.arange(2**200 + shape1_prod, 2**200 + (shape1_prod + shape2_prod)).reshape(shape2)
+            np_hstack = np.hstack((np_a, np_b))
+        else:
+            ak_a = ak.arange(shape1_prod, dtype=dtype).reshape(shape1)
+            ak_b = ak.arange(shape1_prod, (shape1_prod + shape2_prod), dtype=dtype).reshape(shape2)
+            ak_hstack = ak.hstack((ak_a, ak_b))
+
+            np_a = np.arange(shape1_prod, dtype=dtype).reshape(shape1)
+            np_b = np.arange(shape1_prod, (shape1_prod + shape2_prod), dtype=dtype).reshape(shape2)
+            np_hstack = np.hstack((np_a, np_b))
+
+        assert_arkouda_array_equivalent(np_hstack, ak_hstack)
 
     @pytest.mark.parametrize("size", pytest.prob_size)
     @pytest.mark.parametrize("dtype", [int, ak.int64, ak.uint64, float, ak.float64, ak.bigint])
