aecad2589d9f1b88217abed0f6b65c8d3cede1bd
ajpotts <ajpotts@users.noreply.github.com>
2025-06-25 11:21:40 -0400

    Closes #3581:  Update substring_search_benchmark

--- benchmark_v2/substring_search_benchmark.py
+++ benchmark_v2/substring_search_benchmark.py
@@ -2,7 +2,6 @@ import pytest
 
 import arkouda as ak
 
-# stores parameters to pass to str.contains
 SEARCHES = {
     "Non_Regex": ["1 string 1", False],
     "Regex_Literal": ["1 string 1", True],
@@ -11,24 +10,31 @@ SEARCHES = {
 
 
 @pytest.mark.skip_correctness_only(True)
-@pytest.mark.parametrize("s", SEARCHES)
-def bench_substring_search(benchmark, s):
-    cfg = ak.get_config()
-    N = pytest.prob_size * cfg["numLocales"]
+@pytest.mark.skip_numpy(True)
+@pytest.mark.benchmark(group="Arkouda_Strings_SubstringSearch")
+@pytest.mark.parametrize("search_string,use_regex", SEARCHES.values(), ids=list(SEARCHES.keys()))
+def bench_strings_contains(benchmark, search_string, use_regex):
+    N = pytest.prob_size * ak.get_config()["numLocales"]
+    seed = pytest.seed
 
-    start = ak.random_strings_uniform(minlen=1, maxlen=8, size=N, seed=pytest.seed)
-    end = ak.random_strings_uniform(minlen=1, maxlen=8, size=N, seed=pytest.seed)
+    start = ak.random_strings_uniform(minlen=1, maxlen=8, size=N, seed=seed)
+    end = ak.random_strings_uniform(minlen=1, maxlen=8, size=N, seed=seed)
 
     # each string in test_substring contains '1 string 1' with random strings before and after
-    test_substring = start.stick(end, delimiter="1 string 1")
-    nbytes = test_substring.nbytes * test_substring.entry.itemsize
+    a = start.stick(end, delimiter="1 string 1")
 
-    benchmark.pedantic(test_substring.contains, args=SEARCHES[s], rounds=pytest.trials)
+    def run():
+        a.contains(search_string, regex=use_regex)
+        return a.nbytes
 
+    num_bytes = benchmark.pedantic(run, rounds=pytest.trials)
     benchmark.extra_info["description"] = (
-        "Measure the performance of regex and non-regex substring searches."
+        f"Benchmark for Strings.contains with regex={use_regex} and search_string={search_string}"
     )
-    benchmark.extra_info["problem_size"] = pytest.prob_size
+    benchmark.extra_info["problem_size"] = N
+    benchmark.extra_info["backend"] = "Arkouda"
+    benchmark.extra_info["search_string"] = search_string
+    benchmark.extra_info["regex"] = use_regex
     benchmark.extra_info["transfer_rate"] = "{:.4f} GiB/sec".format(
-        (nbytes / benchmark.stats["mean"]) / 2**30
+        (num_bytes / benchmark.stats["mean"]) / 2**30
     )
