f64720c434bf0e98fe814b099a3d8d39ab70d490
drculhane <drculhane@users.noreply.github.com>
2025-08-15 13:14:22 -0400

    First Draft

--- arkouda/numpy/random/__init__.py
+++ arkouda/numpy/random/__init__.py
@@ -1,9 +1,24 @@
 from .generator import Generator, default_rng  # noqa
-from .legacy import randint, standard_normal, uniform
+from .legacy import randint, standard_normal, uniform, choice, seed, integers, \
+            exponential, standard_exponential, logistic, lognormal, normal, permutation, \
+            poisson, random, shuffle, standard_gamma
 
 __all__ = [
     "Generator",
     "randint",
+    "choice",
+    "exponential",
+    "integers",
+    "logistic",
+    "lognormal",
+    "normal",
+    "permutation",
+    "poisson",
+    "random",
+    "seed",
+    "shuffle",
+    "standard_exponential",
+    "standard_gamma",
     "standard_normal",
-    "uniform",
+    "uniform,"
 ]
