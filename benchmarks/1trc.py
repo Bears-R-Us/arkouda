4bcd3149de6718d85aab63b63542e0d199c1f4a3
Eric Vo <eric.aq.vo@gmail.com>
2026-08-14 10:20:32 -0700

    Add 1TRC benchmark skeleton

---
+++ benchmarks/1trc.py
@@ -0,0 +1,12 @@
+import argparse
+
+
+def time_dask_1trc():
+    pass
+
+
+def time_ak_1trc():
+    pass
+
+def create_parser():
+    parser = argparse.ArgumentParser(description="Measure performance of the 1 trillion row challenge.")
